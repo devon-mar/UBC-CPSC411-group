1f55c2ddbc27ec37f5284c69d560bb6c74f2350f
emts <emts@student.ubc.ca>
2023-02-28 13:39:36 -0800

    m4: patch-instructions

--- m2/patch-instructions.rkt
+++ m2/patch-instructions.rkt
@@ -47,14 +47,16 @@
   ;; and the patched 's'
   ;;
   (define (patch-set fn . cvs)
-    (define tmps (current-patch-instructions-registers))
-    (define-values (ls params)
-      (for/fold ([ls '()] [params '()]) ([cv cvs] [tmp tmps])
+    (define cpir (current-patch-instructions-registers))
+    (define-values (ls params _)
+      (for/fold ([ls '()] [params '()] [tmps cpir]) ([cv cvs])
         (define check (car cv))
         (define value (cdr cv))
-        (if (or (eq? check #t) (check value))
-            (values (append-e ls `(set! ,tmp ,value)) (append-e params tmp))
-            (values ls (append-e params value)))))
+        (if (check value)
+            (values (append-e ls `(set! ,(first tmps) ,value))
+                    (append-e params (first tmps))
+                    (rest tmps))
+            (values ls (append-e params value) tmps))))
     (append ls (apply fn params)))
 
   ;; para-asm-lang-v4-s -> (list paren-x64-fvars-v4-s ...)
@@ -286,158 +288,158 @@
       (not (eq? tmp0 tmp1))
       (eq? rax (current-return-value-register))))
 
-    ;; any -> boolean
-    ;; check if symbol is a register in current-patch-instructions-registers 
-    (define (patch-register? r)
-      (and (member r (current-patch-instructions-registers)) #t))
+  ;; any -> boolean
+  ;; check if r is a register in current-patch-instructions-registers
+  (define (patch-register? r)
+    (and (member r (current-patch-instructions-registers)) #t))
 
-    ;; halt - in different places
-    (check-equal?
-      (patch-instructions
-        '(begin
-          (set! fv0 9)
-          (set! rdx L.test.3)
-          (jump rdx)
-          (with-label L.test.1 (with-label L.test.2 (halt rdx)))
-          (with-label L.test.3 (halt 10))
-          (halt fv0)))
+  ;; halt - in different places
+  (check-equal?
+    (patch-instructions
       '(begin
         (set! fv0 9)
         (set! rdx L.test.3)
         (jump rdx)
-        (with-label L.test.1 (with-label L.test.2 (set! rax rdx)))
-        (jump done)
-        (with-label L.test.3 (set! rax 10))
-        (jump done)
-        (set! rax fv0)
-        (jump done)))
+        (with-label L.test.1 (with-label L.test.2 (halt rdx)))
+        (with-label L.test.3 (halt 10))
+        (halt fv0)))
+    '(begin
+      (set! fv0 9)
+      (set! rdx L.test.3)
+      (jump rdx)
+      (with-label L.test.1 (with-label L.test.2 (set! rax rdx)))
+      (jump done)
+      (with-label L.test.3 (set! rax 10))
+      (jump done)
+      (set! rax fv0)
+      (jump done)))
 
-    ;; jump w/ reg & label -- no patch
-    (check-equal?
-      (patch-instructions
-        '(begin
-          (set! r9 0)
-          (with-label L.!!test.1 (jump r9))
-          (jump L.!!test.1)
-          (halt rbx)))
+  ;; jump w/ reg & label -- no patch
+  (check-equal?
+    (patch-instructions
       '(begin
-          (set! r9 0)
-          (with-label L.!!test.1 (jump r9))
-          (jump L.!!test.1)
-          (set! rax rbx)
-          (jump done)))
-    
-    ;; jump w/ fvar -- patch
-    (check-match
-      (patch-instructions
-        '(begin
-          (set! fv0 L.!!test.1)
-          (with-label L.!!test.1 (jump fv0))
-          (halt rbx)))
-      `(begin
-          (set! fv0 L.!!test.1)
-          (with-label L.!!test.1 (set! ,tmp0 fv0))
-          (jump ,tmp0)
-          (set! rax rbx)
-          (jump done))
-      (patch-register? tmp0))
+        (set! r9 0)
+        (with-label L.!!test.1 (jump r9))
+        (jump L.!!test.1)
+        (halt rbx)))
+    '(begin
+        (set! r9 0)
+        (with-label L.!!test.1 (jump r9))
+        (jump L.!!test.1)
+        (set! rax rbx)
+        (jump done)))
+  
+  ;; jump w/ fvar -- patch
+  (check-match
+    (patch-instructions
+      '(begin
+        (set! fv0 L.!!test.1)
+        (with-label L.!!test.1 (jump fv0))
+        (halt rbx)))
+    `(begin
+        (set! fv0 L.!!test.1)
+        (with-label L.!!test.1 (set! ,tmp0 fv0))
+        (jump ,tmp0)
+        (set! rax rbx)
+        (jump done))
+    (patch-register? tmp0))
 
-    ;; with-label
-    (check-match
-      (patch-instructions
-        '(begin
-          (set! fv1 9)
-          (with-label L.test.0 (set! rdx 0))
-          (with-label L.test.1 (set! fv0 fv1))
-          (with-label L.test.2 (halt fv0))))
-      `(begin
+  ;; with-label
+  (check-match
+    (patch-instructions
+      '(begin
         (set! fv1 9)
         (with-label L.test.0 (set! rdx 0))
-        (with-label L.test.1 (set! ,tmp0 fv1))
-        (set! fv0 ,tmp0)
-        (with-label L.test.2 (set! rax fv0))
-        (jump done))
-      (patch-register? tmp0))
-    
-    ;; compare with reg -- no patch
-    (check-equal?
-      (patch-instructions
-        '(begin
-          (set! r13 9)
-          (set! r14 10)
-          (compare r14 0)
-          (compare r13 r14)))
-      `(begin
+        (with-label L.test.1 (set! fv0 fv1))
+        (with-label L.test.2 (halt fv0))))
+    `(begin
+      (set! fv1 9)
+      (with-label L.test.0 (set! rdx 0))
+      (with-label L.test.1 (set! ,tmp0 fv1))
+      (set! fv0 ,tmp0)
+      (with-label L.test.2 (set! rax fv0))
+      (jump done))
+    (patch-register? tmp0))
+  
+  ;; compare with reg -- no patch
+  (check-equal?
+    (patch-instructions
+      '(begin
         (set! r13 9)
         (set! r14 10)
         (compare r14 0)
         (compare r13 r14)))
-    
-    ;; compare with fvar -- patched
-    (check-match
-      (patch-instructions
-        '(begin
-          (set! r13 9)
-          (set! fv1 7)
-          (set! fv2 3)
-          (compare fv1 fv2)
-          (compare fv1 r13)))
-      `(begin
+    `(begin
+      (set! r13 9)
+      (set! r14 10)
+      (compare r14 0)
+      (compare r13 r14)))
+  
+  ;; compare with fvar -- patched
+  (check-match
+    (patch-instructions
+      '(begin
         (set! r13 9)
         (set! fv1 7)
         (set! fv2 3)
-        (set! ,tmp0 fv1)
-        (set! ,tmp1 fv2)
-        (compare ,tmp0 ,tmp1)
-        (set! ,tmp2 fv1)
-        (compare ,tmp2 r13))
-      (and (andmap patch-register? (list tmp0 tmp1 tmp2))
-           (not (equal? tmp0 tmp1))))
+        (compare fv1 fv2)
+        (compare fv1 r13)))
+    `(begin
+      (set! r13 9)
+      (set! fv1 7)
+      (set! fv2 3)
+      (set! ,tmp0 fv1)
+      (set! ,tmp1 fv2)
+      (compare ,tmp0 ,tmp1)
+      (set! ,tmp2 fv1)
+      (compare ,tmp2 r13))
+    (and (andmap patch-register? (list tmp0 tmp1 tmp2))
+         (not (equal? tmp0 tmp1))))
 
-    ;; jump-if with label - no patch
-    (check-equal?
-      (patch-instructions
-        '(begin
-          (set! rcx 0)
-          (compare rcx 1)
-          (jump-if = done)
-          (compare rcx 3)
-          (jump-if >= done)
-          (halt 5)))
+  ;; jump-if with label - no patch
+  (check-equal?
+    (patch-instructions
       '(begin
         (set! rcx 0)
         (compare rcx 1)
         (jump-if = done)
         (compare rcx 3)
         (jump-if >= done)
-        (set! rax 5)
-        (jump done)))
+        (halt 5)))
+    '(begin
+      (set! rcx 0)
+      (compare rcx 1)
+      (jump-if = done)
+      (compare rcx 3)
+      (jump-if >= done)
+      (set! rax 5)
+      (jump done)))
 
-    ;; jump-if with reg & fvar -- patched
-    (check-match
-      (patch-instructions
-        '(begin
-          (set! rcx done)
-          (set! fv0 done)
-          (compare rcx 1)
-          (jump-if < rcx)
-          (compare rcx 2)
-          (jump-if != fv0)
-          (set! rbx 0)))
-      `(begin
+  ;; jump-if with reg & fvar -- patched
+  (check-match
+    (patch-instructions
+      '(begin
         (set! rcx done)
         (set! fv0 done)
         (compare rcx 1)
-        (jump-if >= ,tmpl0)
-        (jump rcx)
-        (with-label ,tmpl0 (set! ,t0 ,t0))
+        (jump-if < rcx)
         (compare rcx 2)
-        (jump-if = ,tmpl1)
-        (set! ,t1 fv0)
-        (jump ,t1)
-        (with-label ,tmpl1 (set! ,t2 ,t2))
-        (set! rbx 0))
-      (and (andmap label? (list tmpl0 tmpl1))
-           (not (equal? tmpl0 tmpl1))
-           (andmap patch-register? (list t0 t1 t2))))
+        (jump-if != fv0)
+        (set! rbx 0)))
+    `(begin
+      (set! rcx done)
+      (set! fv0 done)
+      (compare rcx 1)
+      (jump-if >= ,tmpl0)
+      (jump rcx)
+      (with-label ,tmpl0 (set! ,t0 ,t0))
+      (compare rcx 2)
+      (jump-if = ,tmpl1)
+      (set! ,t1 fv0)
+      (jump ,t1)
+      (with-label ,tmpl1 (set! ,t2 ,t2))
+      (set! rbx 0))
+    (and (andmap label? (list tmpl0 tmpl1))
+         (not (equal? tmpl0 tmpl1))
+         (andmap patch-register? (list t0 t1 t2))))
   )
