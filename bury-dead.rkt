7b40a27abf98a4999c24cf201a43915a16e7932f
Devon Mar <devonm@mdmm.ca>
2023-02-28 10:15:06 -0800

    Fix bury-dead

--- bury-dead.rkt
+++ bury-dead.rkt
@@ -3,15 +3,16 @@
 (require
   cpsc411/compiler-lib
   cpsc411/langs/v2
-  cpsc411/langs/v2-reg-alloc)
+  cpsc411/langs/v2-reg-alloc
+  "undead-analysis.rkt")
 
 
 ;; Milestone 3 - Exercise 6 
 
 ;; Removes assignments to unused abstract locations.
 ;; An assignment to a variable is dead if it is not in the undead-out set for the instruction.
-(define (bury-dead p)
-  (-> asm-lang-v2/undead? asm-lang-v2/locals?)
+(define/contract (bury-dead p)
+  (-> asm-lang-v2/undead? asm-lang-v2/undead?)
 
   ;; Unused
   #;
@@ -54,17 +55,26 @@
           '())]
       ;; Modified template to combine tail effect with other ones.
       [(cons `(begin ,es ...) `(,usts ...))
-       (list `(begin ,@(append-map bury-dead-effect es usts)))]))
+       (define mapped-effects (append-map bury-dead-effect es usts))
+       (if (empty? mapped-effects)
+         '()
+         (list `(begin ,@mapped-effects)))]))
 
   ;; Returns a p with assignments to unused alocs removed.
   ;;
-  ;; asm-lang-v2/undead? -> asm-lang-v2/locals?
-  (define (bury-dead-p p)
+  ;; asm-lang-v2/undead? -> asm-lang-v2/undead?
+  (define/contract (bury-dead-p p)
+    (-> asm-lang-v2/undead? asm-lang-v2/undead?)
     (match p
       [`(module ,info ,tail)
-       `(module
-          ,(info-remove info 'undead-out)
-          ,(bury-dead-tail tail (info-ref info 'undead-out)))]))
+        (define new
+          (undead-analysis
+           `(module
+              ,(info-remove info 'undead-out)
+              ,(bury-dead-tail tail (info-ref info 'undead-out)))))
+        (if (equal? new p)
+          new
+          (bury-dead-p new))]))
 
   (bury-dead-p p))
 
@@ -74,129 +84,211 @@
   ;; 1. Create a asm-lang-v2/undead? program using locals, ust, and p.
   ;; 2. Checks that the output of bury-dead
   ;; 3. Checks that the output program returns 42 when ran.
-  (define (check-42 locals ust p want)
-    (define have
-      (bury-dead `(module ([locals ,locals] [undead-out ,ust]) ,p)))
+  (define (check-42 in want)
+    (define have (bury-dead in))
 
     (check-equal?
       have
-      `(module
-         ([locals ,locals])
-         ,want))
+      want)
 
     (check-equal?
-      (interp-asm-lang-v2/locals have)
+      (interp-asm-lang-v2/undead have)
       42))
 
   ;; nothing to do
   (check-42
-    '()
-    '()
-    '(halt 42)
-    '(halt 42))
+    '(module
+       ([locals ()]
+        [undead-out ()])
+       (halt 42))
+    '(module
+       ([locals ()]
+        [undead-out ()])
+       (halt 42)))
 
   ;; remove one imm mov
   (check-42
-    '(x.1)
-    '(() ())
-    '(begin
-       (set! x.1 42)
-       (halt 42))
-    '(begin
-       (halt 42)))
+    '(module
+       ([locals (x.1)]
+        [undead-out (() ())])
+       (begin
+         (set! x.1 42)
+         (halt 42)))
+    '(module
+       ([locals (x.1)]
+        [undead-out (())])
+       (begin
+         (halt 42))))
+
   ;; no changes needed
   (check-42
-    '(x.1)
-    '((x.1) ())
-    '(begin
-       (set! x.1 42)
-       (halt x.1))
-    '(begin
-       (set! x.1 42)
-       (halt x.1)))
+    '(module
+       ([locals (x.1)]
+        [undead-out ((x.1) ())])
+       (begin
+         (set! x.1 42)
+         (halt x.1)))
+    '(module
+       ([locals (x.1)]
+        [undead-out ((x.1) ())])
+       (begin
+         (set! x.1 42)
+         (halt x.1))))
 
   ;; remove an aloc->aloc mov
   (check-42
-    '(x.1 x.2)
-    '((x.2) () ())
-    '(begin
-       (set! x.2 42)
-       (set! x.1 x.2)
-       (halt 42))
-    '(begin
-       (set! x.2 42)
-       (halt 42)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.2) () ())])
+       (begin
+         (set! x.2 42)
+         (set! x.1 x.2)
+         (halt x.2)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.2) ())])
+       (begin
+         (set! x.2 42)
+         (halt x.2))))
+
+  ;; remove everything (but halt)!
+  (check-42
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.2) () ())])
+       (begin
+         (set! x.2 42)
+         (set! x.1 x.2)
+         (halt 42)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out (())])
+       (begin
+         (halt 42))))
 
   ;; remove a binop
+  ;; and a aloc->aloc mov
   (check-42
-    '(x.1 x.2)
-    '((x.1) (x.2) () ())
-    '(begin
-       (set! x.1 40)
-       (set! x.2 x.1)
-       (set! x.2 (+ x.2 2))
-       (halt 42))
-    '(begin
-       (set! x.1 40)
-       (set! x.2 x.1)
-       (halt 42)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.1) (x.2) () ())])
+       (begin
+         (set! x.1 42)
+         (set! x.2 x.1)
+         (set! x.2 (+ x.2 2))
+         (halt x.1)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.1) ())])
+       (begin
+         (set! x.1 42)
+         (halt x.1))))
+
+  ;; remove everything (but halt)!
+  (check-42
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.1) (x.2) () ())])
+       (begin
+         (set! x.1 42)
+         (set! x.2 x.1)
+         (set! x.2 (+ x.2 2))
+         (halt 42)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out (())])
+       (begin
+         (halt 42))))
+
   ;; leave the binop alone!
   (check-42
-    '(x.1 x.2)
-    '((x.1) (x.2) (x.2) ())
-    '(begin
-       (set! x.1 40)
-       (set! x.2 x.1)
-       (set! x.2 (+ x.2 2))
-       (halt x.2))
-    '(begin
-       (set! x.1 40)
-       (set! x.2 x.1)
-       (set! x.2 (+ x.2 2))
-       (halt x.2)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.1) (x.2) (x.2) ())])
+       (begin
+         (set! x.1 40)
+         (set! x.2 x.1)
+         (set! x.2 (+ x.2 2))
+         (halt x.2)))
+    '(module
+       ([locals (x.1 x.2)]
+        [undead-out ((x.1) (x.2) (x.2) ())])
+       (begin
+         (set! x.1 40)
+         (set! x.2 x.1)
+         (set! x.2 (+ x.2 2))
+         (halt x.2))))
 
   ;; lots of nesting with removals
   (check-42
-    '(x.1 x.2 x.3)
-    '((()) (() (x.2) (x.2) ()))
-    '(begin
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out ((()) (() (x.2) (x.2) ()))])
        (begin
-         (set! x.1 2))
+         (begin
+           (set! x.1 2))
+         (begin
+           (set! x.3 3)
+           (set! x.2 40)
+           (set! x.2 (+ x.2 2))
+           (halt x.2))))
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out (((x.2) (x.2) ()))])
        (begin
-         (set! x.3 3)
-         (set! x.2 40)
-         (set! x.2 (+ 40 2))
-         (halt 42)))
-    '(begin
-       (begin)
+         (begin
+           (set! x.2 40)
+           (set! x.2 (+ x.2 2))
+           (halt x.2)))))
+
+  ;; remove everything except nested halt
+  (check-42
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out ((()) (() (x.2) (x.2) ()))])
        (begin
-         (set! x.2 40)
-         (set! x.2 (+ 40 2))
-         (halt 42))))
+         (begin
+           (set! x.1 2))
+         (begin
+           (set! x.3 3)
+           (set! x.2 40)
+           (set! x.2 (+ x.2 2))
+           (halt 42))))
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out ((()))])
+       (begin
+         (begin
+           (halt 42)))))
+
   ;; lots of nesting with nothing to remove
   (check-42
-    '(x.1 x.2 x.3)
-    '(((x.1)) ((x.3 x.1) (x.3 x.1) (x.1) (x.1 x.2) (x.2) (x.2) ()))
-    '(begin
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out (((x.1)) ((x.3 x.1) (x.3 x.1) (x.1) (x.1 x.2) (x.2) (x.2) ()))])
        (begin
-         (set! x.1 1))
+         (begin
+           (set! x.1 1))
+         (begin
+           (set! x.3 1)
+           (set! x.3 (* x.3 2))
+           (set! x.1 (+ x.1 x.3))
+           (set! x.2 38)
+           (set! x.2 (+ x.2 x.1))
+           (set! x.2 (+ x.2 1))
+           (halt x.2))))
+    '(module
+       ([locals (x.1 x.2 x.3)]
+        [undead-out (((x.1)) ((x.3 x.1) (x.3 x.1) (x.1) (x.1 x.2) (x.2) (x.2) ()))])
        (begin
-         (set! x.3 1)
-         (set! x.3 (* x.3 2))
-         (set! x.1 (+ x.1 x.3))
-         (set! x.2 38)
-         (set! x.2 (+ x.2 x.1))
-         (set! x.2 (+ x.2 1))
-         (halt x.2)))
-    '(begin
-       (begin
-         (set! x.1 1))
-       (begin
-         (set! x.3 1)
-         (set! x.3 (* x.3 2))
-         (set! x.1 (+ x.1 x.3))
-         (set! x.2 38)
-         (set! x.2 (+ x.2 x.1))
-         (set! x.2 (+ x.2 1))
-         (halt x.2))))
+         (begin
+           (set! x.1 1))
+         (begin
+           (set! x.3 1)
+           (set! x.3 (* x.3 2))
+           (set! x.1 (+ x.1 x.3))
+           (set! x.2 38)
+           (set! x.2 (+ x.2 x.1))
+           (set! x.2 (+ x.2 1))
+           (halt x.2)))))
   )
