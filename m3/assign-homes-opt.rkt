14e1b604ed29b672069a585890c5a49c8e145fe7
emts <emts@student.ubc.ca>
2023-03-05 14:29:28 -0800

    m4: assign-homes-opt

--- m3/assign-homes-opt.rkt
+++ m3/assign-homes-opt.rkt
@@ -18,23 +18,62 @@
 
 (module+ test
   (require rackunit)
-  (check-equal? (assign-homes-opt '(module ()
-                                           (begin
-                                             (set! x.1 0)
-                                             (halt x.1))))
-                `(begin
-                   (set! r15 0)
-                   (halt r15)))
-  (check-equal? (assign-homes-opt '(module ()
-                                           (begin
-                                             (set! x.1 0)
-                                             (set! y.1 x.1)
-                                             (set! w.1 1)
-                                             (set! w.1 (+ w.1 y.1))
-                                             (halt w.1))))
-                '(begin
-                   (set! r15 0)
-                   (set! r15 r15)
-                   (set! r14 1)
-                   (set! r14 (+ r14 r15))
-                   (halt r14))))
+
+  (check-equal?
+    (assign-homes-opt
+      '(module ()
+        (begin
+          (set! x.1 0)
+          (halt x.1))))
+    `(module
+      (begin
+        (set! r15 0)
+        (halt r15))))
+
+  ;; sets
+  (check-equal?
+    (assign-homes-opt
+      '(module ()
+        (begin
+          (set! x.1 0)
+          (set! y.1 x.1)
+          (set! w.1 1)
+          (set! w.1 (+ w.1 y.1))
+          (halt w.1))))
+    '(module
+      (begin
+        (set! r15 0)
+        (set! r15 r15)
+        (set! r14 1)
+        (set! r14 (+ r14 r15))
+        (halt r14))))
+
+  ;; if and pred
+  (check-match
+    (assign-homes-opt
+      '(module ()
+        (begin
+          (set! u.1 9)
+          (set! x.1 u.1)
+          (begin
+            (if (= x.1 2)
+                (set! z.1 9)
+                (set! y.1 x.1)))
+          (if (begin (set! j.1 17) (set! j.1 (+ j.1 z.1)) (set! i.1 y.1) (= j.1 y.1))
+              (halt x.1)
+              (halt i.1)))))
+    `(module
+      (begin
+        (set! ,r0 9)
+        (set! ,r1 ,r0)
+        (begin (if (= ,r1 2) (set! ,r3 9) (set! ,r2 ,r1)))
+        (if (begin (set! ,r5 17) (set! ,r5 (+ ,r5 ,r3)) (set! ,r4 ,r2) (= ,r5 ,r2))
+            (halt ,r1)
+            (halt ,r4))))
+    (and (andmap register? (list r0 r1 r2 r3 r4 r5))
+         (not (member r0 '(r2 r3)))
+         (not (member r1 '(r2 r3 r4 r5)))
+         (not (member r2 '(r3 r5)))
+         (not (equal? r3 r5))
+         (not (equal? r4 r5))))
+  )
