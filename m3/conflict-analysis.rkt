ef76b460c1ac1e94bb5d30eb5fdd91db243a5115
emts <emts@student.ubc.ca>
2023-03-05 13:00:36 -0800

    m4: conflict-analysis

--- m3/conflict-analysis.rkt
+++ m3/conflict-analysis.rkt
@@ -2,45 +2,83 @@
 
 (require
   cpsc411/compiler-lib
-  cpsc411/graph-lib)
+  cpsc411/graph-lib
+  cpsc411/langs/v4
+  "../utils/compiler-utils.rkt")
 
 (provide conflict-analysis)
 
-;; M3 Exercise 2
-;; Asm-lang-v2/undead -> Asm-lang-v2/conflicts
+;; Milestone 3 Exercise 2
+;; Milestone 4 Exercise 14
+;;
 ;; Create a conflict graph for the Asm-lang program with the undead-set tree
-(define (conflict-analysis p)
-  ;; (Asm-lang-v2/undead tail) undead-set-tree graph -> graph
-  ;; Returns the updated conflict graph for the alocs in the tail
-  (define (conflict-tail tail ust graph)
+(define/contract (conflict-analysis p)
+  (-> asm-pred-lang-v4/undead? asm-pred-lang-v4/conflicts?)
+
+  (define conflict-graph (new-graph))
+
+  ;; Updates conflict graph for the alocs in the tail
+  ;; tail undead-set-tree -> void
+  (define (conflict-tail tail ust)
     (match (cons tail ust)
-      [(cons `(halt ,_) _) graph]
+      [(cons `(halt ,_) _) (void)]
       [(cons `(begin ,effects ... ,tail) `(,usts ...))
-       (define g (for/fold ([g graph]) ([u usts] [e effects])
-         (conflict-effect e u g)))
-       (conflict-tail tail (last ust) g)]))
+       (for ([e effects] [u usts])
+         (conflict-effect e u))
+       (conflict-tail tail (last ust))]
+      [(cons `(if ,pred ,tail1 ,tail2) `(,ustp ,ust1 ,ust2))
+       (conflict-pred pred ustp)
+       (conflict-tail tail1 ust1)
+       (conflict-tail tail2 ust2)]))
   
-  ;; (Asm-lang-v2/undead effect) undead-set-tree graph -> graph
-  ;; Returns the updated conflict graph for the alocs in the effect
-  (define (conflict-effect effect ust graph)
+  ;; Updates conflict graph for the alocs in the effect
+  ;; effect undead-set-tree -> void
+  (define (conflict-effect effect ust)
     (match (cons effect ust)
-      [(cons `(set! ,aloc ,_) undead-out)
-       ;; for both:
-       ;; (set! aloc triv)
-       ;; (set! aloc_1 (binop aloc_1 triv))
-       (for/fold ([g graph]) ([u undead-out])
-         (if (equal? aloc u)
-             g
-             (add-edge g aloc u)))]
+      [(cons `(set! ,aloc (,_ ,aloc ,_)) undead-out)
+       (for ([u undead-out])
+         (unless
+          (equal? aloc u)
+          (set! conflict-graph (add-edge conflict-graph aloc u))))]
+      [(cons `(set! ,aloc ,triv) undead-out)
+       (for ([u undead-out])
+         (unless
+          (or (equal? aloc u) (and (aloc? triv) (equal? triv u)))
+          (set! conflict-graph (add-edge conflict-graph aloc u))))]
       [(cons `(begin ,effects ...) `(,usts ...))
-       (for/fold ([g graph]) ([u usts] [e effects])
-         (conflict-effect e u g))]))
+       (for ([e effects] [u usts])
+         (conflict-effect e u))]
+      [(cons `(if ,pred ,effect1 ,effect2) `(,ustp ,ust1 ,ust2))
+       (conflict-pred pred ustp)
+       (conflict-effect effect1 ust1)
+       (conflict-effect effect2 ust2)]))
+
+  ;; Updates conflict graph for the alocs in the pred
+  ;; pred undead-set-tree -> void
+  (define (conflict-pred pred ust)
+    (match (cons pred ust)
+      [(cons `(true) _)
+       (void)]
+ 	 	  [(cons `(false) _)
+       (void)]
+ 	 	  [(cons `(not ,ipred) iust)
+       (conflict-pred ipred iust)]
+ 	 	  [(cons `(begin ,effect ... ,tpred) `(,usts ...))
+       (for ([e effect] [u usts])
+        (conflict-effect e u))
+       (conflict-pred tpred (last usts))]
+ 	 	  [(cons `(if ,ppred ,pred1 ,pred2) `(,ustp ,ust1 ,ust2))
+       (conflict-pred ppred ustp)
+       (conflict-pred pred1 ust1)
+       (conflict-pred pred2 ust2)]
+      [(cons `(,relop ,_ ,_) _)
+       #:when(relop? relop)
+       (void)]))
 
   (match p
     [`(module ,info ,tail)
-     (define init-graph (new-graph (info-ref info 'locals)))
-     (define conflict-graph
-       (conflict-tail tail (info-ref info 'undead-out) init-graph))
+     (set! conflict-graph (new-graph (info-ref info 'locals)))
+     (conflict-tail tail (info-ref info 'undead-out))
      (define newinfo
        (info-remove (info-set info 'conflicts conflict-graph) 'undead-out))
      `(module ,newinfo ,tail)]))
@@ -84,101 +122,96 @@
   (check-false (graph-equals?
     '((x.1 (x.2 x.3)) (x.2 (x.1 x.3)) (x.3 (x.4 x.2)) (x.4 (x.3)))
     '((x.1 (x.2 x.3)) (x.2 (x.1 x.3)) (x.3 (x.4)) (x.4 (x.3)))))
+
+  ;; Check that the graphs are equal
+  ;; any any -> void
+  (define-binary-check (check-graph? actual expected)
+    (graph-equals? actual expected))
+
+  ;; Check that a program w/ undead-out compiles into a program w/ conflicts
+  ;; by compiling with conflict-analysis
+  ;; asm-pred-lang-v4/locals undead-set-tree conflicts -> void
+  (define-check (check-conflict program undead-out conflicts)
+    (define-values (compiled etail)
+      (match program
+        [`(module ,info ,etail)
+          (values
+            (conflict-analysis
+              `(module ,(info-set info 'undead-out undead-out) ,etail))
+            etail)]))
+    (match compiled
+      [`(module ,info ,atail)
+       (check-equal? etail atail)
+       (check-graph? (info-ref info 'conflicts) conflicts)]))
   
   ;; conflict-analysis tests
-  (check-equal?
-    (conflict-analysis
-      '(module
-        ((locals ()) (undead-out (())))
-        (begin (halt 0))))
+  (check-conflict
     '(module
-        ((locals ()) (conflicts ()))
-        (begin (halt 0))))
+        ((locals ()))
+        (begin (halt 0)))
+    '(())
+    '())
 
   ; No conflicts
-  (check-match
-    (conflict-analysis
-      '(module
-        ((locals (x.1 x.2)) (undead-out ((x.2) (x.1) ())))
-        (begin (set! x.2 12) (set! x.1 x.2) (halt x.1))))
-    `(module
-        ((locals (x.1 x.2)) (conflicts ,conflicts))
+  (check-conflict
+    '(module
+        ((locals (x.1 x.2)))
         (begin (set! x.2 12) (set! x.1 x.2) (halt x.1)))
-    (graph-equals? conflicts '((x.1 ()) (x.2 ()))))
+    '((x.2) (x.1) ())
+    '((x.1 ()) (x.2 ())))
 
   ; One-to-one conflict
-  (check-match
-    (conflict-analysis
-      '(module
-        ((locals (x.1 x.2))
-         (undead-out
-          ((x.1)
-           ((x.1 x.2)
-            ())
-           ())))
+  (check-conflict
+    '(module
+        ((locals (x.1 x.2)))
         (begin
           (set! x.1 5)
           (begin
             (set! x.2 10)
             (set! x.1 (+ x.1 x.2)))
-          (halt 0))))
-    `(module
-      ((locals (x.1 x.2)) (conflicts ,conflicts))
-      (begin
-        (set! x.1 5)
+          (halt 0)))
+    '((x.1)
+      ((x.1 x.2)
+       ())
+      ())
+    '((x.1 (x.2)) (x.2 (x.1))))
+
+  ; Move optimization
+  (check-conflict
+    '(module
+        ((locals (x.1 x.2 x.3)))
         (begin
-          (set! x.2 10)
-          (set! x.1 (+ x.1 x.2)))
-        (halt 0)))
-    (graph-equals? conflicts '((x.1 (x.2)) (x.2 (x.1)))))
+          (set! x.1 5)
+          (set! x.2 x.1)
+          (set! x.3 x.1)
+          (halt x.2)))
+    '((x.1)
+      (x.1 x.2)
+      (x.2)
+      ())
+    '((x.1 ()) (x.2 (x.3)) (x.3 (x.2))))
+  
+  ; No move optimization for binop
+  (check-conflict
+    '(module
+        ((locals (x.1 x.2 x.3)))
+        (begin
+          (set! x.1 5)
+          (set! x.2 x.1)
+          (set! x.2 (+ x.2 x.1))
+          (set! x.3 x.1)
+          (halt x.2)))
+    '((x.1)
+      (x.1 x.2)
+      (x.1 x.2)
+      (x.2)
+      ())
+    '((x.1 (x.2)) (x.2 (x.1 x.3)) (x.3 (x.2))))
 
   ; Many alocs + conflicts
-  (check-match
-    (conflict-analysis
-      '(module
-        ((locals (v.1 w.2 x.3 y.4 z.5 t.6 p.1 a.10))
-         (undead-out
-           ((v.1)
-            (v.1 w.2)
-            ((w.2 x.3))
-            (p.1 w.2 x.3)
-            (w.2 x.3)
-            (y.4 w.2 x.3)
-            ((p.1 y.4 w.2 x.3)
-             ((y.4 w.2 x.3)
-              (z.5 y.4 w.2)
-              (z.5 y.4)))
-            (t.6 z.5)
-            (t.6 z.5 p.1)
-            (t.6 z.5 p.1 a.10)
-            (t.6 z.5 p.1)
-            ((t.6 z.5)
-             (z.5)
-             (())))))
-        (begin
-          (set! v.1 1)
-          (set! w.2 46)
-          (begin (set! x.3 v.1))
-          (set! p.1 7)
-          (set! x.3 (+ x.3 p.1))
-          (set! y.4 x.3)
-          (begin
-            (set! p.1 4)
-            (begin
-              (set! y.4 (+ y.4 p.1))
-              (set! z.5 x.3)
-              (set! z.5 (+ z.5 w.2))))
-          (set! t.6 y.4)
-          (set! p.1 -1)
-          (set! a.10 10)
-          (set! a.10 (* a.10 10))
-          (begin
-            (set! t.6 (* t.6 p.1))
-            (set! z.5 (+ z.5 t.6))
-            (begin (halt z.5))))))
-    `(module
-      ((locals (v.1 w.2 x.3 y.4 z.5 t.6 p.1 a.10))
-       (conflicts ,conflicts))
+  (check-conflict
+    '(module
+      ((locals (v.1 w.2 x.3 y.4 z.5 t.6 p.1 a.10)))
       (begin
         (set! v.1 1)
         (set! w.2 46)
@@ -200,16 +233,113 @@
           (set! t.6 (* t.6 p.1))
           (set! z.5 (+ z.5 t.6))
           (begin (halt z.5)))))
-    (graph-equals?
-      conflicts
-      '((v.1 (w.2))
-        (w.2 (z.5 y.4 p.1 x.3 v.1))
-        (x.3 (y.4 p.1 w.2))
-        (y.4 (z.5 x.3 p.1 w.2))
-        (z.5 (p.1 t.6 w.2 y.4 a.10))
-        (t.6 (p.1 z.5 a.10))
-        (p.1 (z.5 t.6 y.4 x.3 w.2 a.10))
-        (a.10 (t.6 z.5 p.1)))))
+    '((v.1)
+      (v.1 w.2)
+      ((w.2 x.3))
+      (p.1 w.2 x.3)
+      (w.2 x.3)
+      (y.4 w.2 x.3)
+      ((p.1 y.4 w.2 x.3)
+       ((y.4 w.2 x.3)
+        (z.5 y.4 w.2)
+        (z.5 y.4)))
+      (t.6 z.5)
+      (t.6 z.5 p.1)
+      (t.6 z.5 p.1 a.10)
+      (t.6 z.5 p.1)
+      ((t.6 z.5)
+       (z.5)
+       (())))
+    '((v.1 (w.2))
+      (w.2 (z.5 y.4 p.1 x.3 v.1))
+      (x.3 (y.4 p.1 w.2))
+      (y.4 (z.5 x.3 p.1 w.2))
+      (z.5 (p.1 t.6 w.2 y.4 a.10))
+      (t.6 (p.1 z.5 a.10))
+      (p.1 (z.5 t.6 y.4 x.3 w.2 a.10))
+      (a.10 (t.6 z.5 p.1))))
 
-  )
+  ; if in tail
+  (check-conflict
+    '(module
+      ((locals (x.1 x.2 x.3)))
+      (begin
+        (set! x.1 5)
+        (if (= x.1 2)
+          (begin (set! x.2 10) (set! x.1 (+ x.1 x.2)) (halt x.1))
+          (begin (set! x.3 9) (set! x.1 x.3) (halt x.1)))))
+    '((x.1)
+      ((x.1)
+       ((x.1 x.2) (x.1) ())
+       ((x.3) (x.1) ())))
+    '((x.1 (x.2)) (x.2 (x.1)) (x.3 ())))
   
+   ; if in effect
+  (check-conflict
+    '(module
+      ((locals (x.1 x.2 x.3)))
+      (begin
+        (set! x.1 5)
+        (if (not (>= x.1 2))
+          (begin (set! x.3 9) (set! x.1 x.3))
+          (begin (set! x.2 10) (set! x.1 (* x.1 x.2))))
+        (halt x.1)))
+    '((x.1)
+      ((x.1)
+       ((x.3) (x.1))
+       ((x.1 x.2) (x.1)))
+      ())
+    '((x.1 (x.2)) (x.2 (x.1)) (x.3 ())))
+  
+  ;; if in pred
+  (check-conflict
+    '(module
+      ((locals (x.1 x.2 x.3 x.4 x.5 x.6 x.7)))
+      (begin
+        (set! x.6 0)
+        (set! x.1 5)
+        (set! x.2 8)
+        (set! x.4 x.2)
+        (if
+          (not
+            (begin
+              (set! x.4 (+ x.4 9))
+              (if (= x.2 x.4)
+                  (begin
+                    (set! x.3 9)
+                    (set! x.1 x.3)
+                    (set! x.5 9)
+                    (= x.1 x.5))
+                  (begin
+                    (set! x.3 10)
+                    (set! x.4 (* x.4 9))
+                    (set! x.7 x.3)
+                    (set! x.1 (* x.1 x.3))
+                    (not (< x.4 x.7))))))
+          (halt x.2)
+          (halt x.3))))
+    '(()
+      (x.1)
+      (x.1 x.2)
+      (x.4 x.1 x.2)
+      (((x.4 x.1 x.2)
+        ((x.4 x.1 x.2)
+         ((x.3 x.2)
+          (x.1 x.3 x.2)
+          (x.5 x.1 x.3 x.2)
+          (x.3 x.2))
+         ((x.1 x.4 x.3 x.2)
+          (x.1 x.4 x.3 x.2)
+          (x.1 x.7 x.4 x.3 x.2)
+          (x.7 x.4 x.3 x.2)
+          (x.3 x.2))))
+       ()
+       ()))
+    '((x.1 (x.2 x.3 x.4 x.5 x.7))
+      (x.2 (x.1 x.3 x.4 x.5 x.7))
+      (x.3 (x.1 x.2 x.4 x.5))
+      (x.4 (x.1 x.2 x.3 x.7))
+      (x.5 (x.1 x.2 x.3))
+      (x.6 ())
+      (x.7 (x.1 x.2 x.4))))
+  )
