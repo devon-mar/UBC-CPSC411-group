2f15ccbe69f6eb8d75c52a5b822b765ecbf21ad0
emts <emts@student.ubc.ca>
2023-03-05 16:00:06 -0800

    m4: util tests

--- utils/gen-utils.rkt
+++ utils/gen-utils.rkt
@@ -8,3 +8,10 @@
 ;; Append element to end of list
 (define (append-e l e)
   (append l (list e)))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (append-e '() 1) '(1))
+  (check-equal? (append-e '(a b c) 'd) '(a b c d))
+  (check-equal? (append-e '(a b c) '(d e f)) '(a b c (d e f)))
+  )
