89edbb853ded5799fe5fbb96509aff7b9772b0fc
Devon Mar <devonm@mdmm.ca>
2023-03-11 21:40:53 -0800

    Update M5 templates

--- m5/templates.rkt
+++ m5/templates.rkt
@@ -224,3 +224,170 @@
       ['!= (void)]))
 
   (void))
+
+(define/contract (imp-cmf-lang-v5-template p)
+  (-> imp-cmf-lang-v5? any/c)
+
+  (define (imp-cmf-lang-v5-template-p p)
+    (match p
+      [`(module (define ,labels ,tails) ... ,tail)
+        (void)]))
+
+  (define (imp-cmf-lang-v5-template-pred p)
+    (match p
+      [`(true)
+        (void)]
+      [`(false)
+        (void)]
+      [`(not ,p)
+        (void)]
+      [`(begin ,es ... ,p)
+        (void)]
+      [`(if ,p1 ,p2 ,p3)
+        (void)]
+      [`(,r ,o1 ,o2)
+        (void)]))
+
+  (define (imp-cmf-lang-v5-template-tail t)
+    (match t
+      [`(jump ,trg ,locs ...)
+        (void)]
+      [`(begin ,es ... ,tail)
+        (void)]
+      [`(if ,p ,t1 ,t2)
+        (void)]
+      [value (void)]))
+
+  (define (imp-cmf-lang-v5-template-value v)
+    (match v
+      [`(,b ,o1 ,o2) 
+        (void)]
+      [triv (void)]))
+
+  (define (imp-cmf-lang-v5-template-effect e)
+    (match e
+      [`(set! ,l ,v)
+        (void)]
+      [`(begin ,es ... ,e)
+        (void)]
+      [`(if ,p ,e1 ,e2)
+        (void)]))
+
+  (define (imp-cmf-lang-v5-template-opand o)
+    (match o
+      [(? int64?)
+       (void)]
+      [loc (void)]))
+
+  (define (imp-cmf-lang-v5-template-loc l)
+    (match l
+      [(? aloc?)
+       (void)]
+      [rloc (void)]))
+
+  (define (imp-cmf-lang-v5-template-trg t)
+    (match t
+      [(? label?)
+       (void)]
+      [loc (void)]))
+
+  (define (imp-cmf-lang-v5-template-binop b)
+    (match b
+      ['* (void)]
+      ['+ (void)]))
+  
+  (define (imp-cmf-lang-v5-template-relop r)
+    (match r
+      ['< (void)]
+      ['<= (void)]
+      ['= (void)]
+      ['>= (void)]
+      ['> (void)]
+      ['!= (void)]))
+
+  (void))
+
+(define/contract (asm-pred-lang-v5-template p)
+  (-> asm-pred-lang-v5? any/c)
+
+  (define (asm-pred-lang-v5-template-p p)
+    (match p
+      [`(module ,info (define ,labels ,infos ,tails) ... ,tail)
+        (void)]))
+
+  (define (asm-pred-lang-v5-template-pred p)
+    (match p
+      [`(true)
+        (void)]
+      [`(false)
+        (void)]
+      [`(not ,p)
+        (void)]
+      [`(begin ,es ... ,p)
+        (void)]
+      [`(if ,p1 ,p2 ,p3)
+        (void)]
+      [`(,r ,l ,o)
+        (void)]))
+
+  (define (asm-pred-lang-v5-template-tail t)
+    (match t
+      [`(halt ,o)
+        (void)]
+      [`(jump ,trg ,locs ...)
+        (void)]
+      [`(begin ,es ... ,t)
+        (void)]
+      [`(if ,p ,t1 ,t2)
+        (void)]))
+
+  (define (asm-pred-lang-v5-template-effect e)
+    (match e
+      [`(set! ,loc ,triv)
+        (void)]
+      [`(set! ,loc (,binop ,loc ,opand))
+        (void)]
+      [`(begin ,es ... ,e)
+        (void)]
+      [`(if ,p ,e1 ,e2)
+        (void)]))
+
+  (define (asm-pred-lang-v5-template-opand o)
+    (match o
+      [(? int64?)
+       (void)]
+      [loc (void)]))
+
+  (define (asm-pred-lang-v5-template-triv t)
+    (match t
+      [(? label?)
+       (void)]
+      [opand (void)]))
+
+  (define (asm-pred-lang-v5-template-loc l)
+    (match l
+      [(? aloc?)
+       (void)]
+      [rloc (void)]))
+
+  (define (asm-pred-lang-v5-template-trg t)
+    (match t
+      [(? label?)
+       (void)]
+      [loc (void)]))
+
+  (define (asm-pred-lang-v5-template-binop b)
+    (match b
+      ['* (void)]
+      ['+ (void)]))
+  
+  (define (asm-pred-lang-v5-template-relop r)
+    (match r
+      ['< (void)]
+      ['<= (void)]
+      ['= (void)]
+      ['>= (void)]
+      ['> (void)]
+      ['!= (void)]))
+
+  (void))
