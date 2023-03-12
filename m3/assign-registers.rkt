019be928cfdd5f888d039c813787c312d90cf15b
emts <emts@student.ubc.ca>
2023-03-12 10:43:48 -0700

    m5: assign-registers

--- m3/assign-registers.rkt
+++ m3/assign-registers.rkt
@@ -1,19 +1,24 @@
 #lang racket
 
-(require cpsc411/compiler-lib
-         cpsc411/graph-lib)
+(require
+  cpsc411/compiler-lib
+  cpsc411/graph-lib
+  cpsc411/langs/v5)
+
 (provide assign-registers)
 
 ;; Milestone 3 Exercise #3
 ;; Milestone 4 Exercise 13
+;; Milestone 5 Exercise 10
 ;;
-;; asm-pred-lang-v4/conflicts -> asm-pred-lang-v4/assignments
 ;; Assign register or frame location to each local variable
 ;; Assumes current-assignable-registers parameter to be set
-(define (assign-registers p)
+(define/contract (assign-registers p)
+  (-> asm-pred-lang-v5/conflicts? asm-pred-lang-v5/assignments?)
+
   (define fvar-counter 0)
 
-  ;; (list aloc ...) (list (aloc (aloc ...))) -> (values aloc (list aloc ...) (list (aloc (aloc...))...))
+  ;; (list aloc ...) (list (aloc (loc ...))) -> (values aloc (list aloc ...) (list (aloc (aloc...))...))
   ;; removes low degree abstract location from aloc-list and return tuple of
   ;; abstract location, the aloc-list without the abstract location, and conf-list without the
   ;; abstract location
@@ -25,15 +30,17 @@
     (define removed (car sorted))
     (values removed (remove removed aloc-list) (remove-vertex conf-list removed)))
 
-  ;; aloc ((aloc loc)...) -> ((aloc loc) ...)
+  ;; aloc ((aloc loc)...) (list fvar) -> ((aloc loc) ...)
   ;; generates new aloc assignment by mapping aloc to an fvar
-  (define (assign-aloc-to-fvar aloc prev-assignment)
+  (define (assign-aloc-to-fvar aloc prev-assignment blacklist-fvars)
     (define fvar
       (make-fvar (begin0 fvar-counter
                    (set! fvar-counter (+ 1 fvar-counter)))))
-    (cons (list aloc fvar) prev-assignment))
+    (if (set-member? blacklist-fvars fvar)
+        (assign-aloc-to-fvar aloc prev-assignment blacklist-fvars)
+        (cons (list aloc fvar) prev-assignment)))
 
-  ;; aloc ((aloc loc)...) -> ((aloc loc) ...)
+  ;; aloc ((aloc loc)...) (list register) -> ((aloc loc) ...)
   ;; generates new aloc assignment by mapping aloc to a register in the whitelist
   (define (assign-aloc-to-reg aloc prev-assignment whitelist-regs)
     (cons (list aloc (first whitelist-regs)) prev-assignment))
@@ -43,15 +50,21 @@
   ;; its conflict list
   (define (assign-aloc aloc conf-list prev-assignment)
     (define conflicts (get-neighbors conf-list aloc))
-    (define blacklist-regs
-      (for/fold ([reg-list '()]) ([c conflicts])
-        ;; If conflict mapped to reg, get reg. Else empty list
-        (define reg
-          (if (register? (first (dict-ref prev-assignment c))) (dict-ref prev-assignment c) '()))
-        (append reg-list reg)))
+    (define-values (blacklist-regs blacklist-fvars)
+      (for/fold ([reg-list '()] [fvar-list '()]) ([c conflicts])
+        (match c
+          [(? register?) (values (append reg-list (list c)) fvar-list)]
+          [(? fvar?) (values reg-list (append fvar-list (list c)))]
+          [(? aloc?)
+           (define prev (first (dict-ref prev-assignment c)))
+           (if (register? prev)
+               (values (append (list prev) reg-list) fvar-list)
+               ;; we don't need to add fvars from prev-assignment
+               ;; since assignment fvars are always unique to the next assignment
+               (values reg-list fvar-list))])))
     (define whitelist-regs (set-subtract (current-assignable-registers) blacklist-regs))
     (if (empty? whitelist-regs)
-        (assign-aloc-to-fvar aloc prev-assignment)
+        (assign-aloc-to-fvar aloc prev-assignment blacklist-fvars)
         (assign-aloc-to-reg aloc prev-assignment whitelist-regs)))
 
   ;; aloc-list conf-list -> aloc assignment
@@ -65,6 +78,8 @@
        (define new-assignment (generate-assignment new-aloc-list new-conf-list))
        (assign-aloc pop-aloc conf-list new-assignment)]))
 
+  ;; info -> info
+  ;; Takes info with locals & conflicts and returns info with assignments
   (define (convert-info info)
     (define local-list (info-ref info 'locals))
     (define conflicts (info-ref info 'conflicts))
@@ -144,4 +159,34 @@
                 '(module ((locals (x.1)) (conflicts ((x.1 ()))) (assignment ((x.1 fv0))))
                          (begin
                            (set! x.1 42)
-                           (halt x.1)))))
+                           (halt x.1))))
+
+  ;; check does not assign to conflicting register
+  (check-match
+    (assign-registers
+      '(module ((locals (x.1 x.2))
+                (conflicts ((x.1 (fv0 r15 x.2)) (r15 (x.1 x.2)) (fv0 (x.1)) (x.2 (r15 x.1)))))
+          (halt 0)))
+    `(module ((locals (x.1 x.2))
+              (conflicts ((x.1 (fv0 r15 x.2)) (r15 (x.1 x.2)) (fv0 (x.1)) (x.2 (r15 x.1))))
+              (assignment ,assignment))
+      (halt 0))
+    (and (= (length assignment) 2)
+         (equal? (dict-ref assignment 'x.2) '(r13))
+         (equal? (dict-ref assignment 'x.1) '(r14))))
+
+  ;; check does not assign to conflicting fvar
+  (check-match
+    (parameterize ([current-assignable-registers '()])
+      (assign-registers
+        '(module ((locals (x.1 x.2))
+                  (conflicts ((x.1 (fv0 rdx)) (x.2 (fv2)) (rdx (x.1)) (fv0 (x.1)) (fv2 (x.2)))))
+            (halt 2))))
+    `(module ((locals (x.1 x.2))
+              (conflicts ((x.1 (fv0 rdx)) (x.2 (fv2)) (rdx (x.1)) (fv0 (x.1)) (fv2 (x.2))))
+              (assignment ,assignment))
+      (halt 2))
+    (and (= (length assignment) 2)
+         (equal? (dict-ref assignment 'x.2) '(fv3))
+         (equal? (dict-ref assignment 'x.1) '(fv1))))
+  )
