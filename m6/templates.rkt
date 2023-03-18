39103963e16260436f0a98548e07582d9d6f56e0
Devon Mar <devonm@mdmm.ca>
2023-03-18 14:52:04 -0700

    Update templates

--- m6/templates.rkt
+++ m6/templates.rkt
@@ -88,3 +88,89 @@
       ['!= (void)]))
 
   (void))
+
+(define/contract (asm-pred-lang-v6-template p)
+  (-> asm-pred-lang-v6? any/c)
+
+  (define (asm-pred-lang-v6-template-p p)
+    (match p
+      [`(module ,info (define ,labels ,infos ,tails) ... ,tail)
+        (void)]))
+
+  (define (asm-pred-lang-v6-template-pred p)
+    (match p
+      [`(true)
+        (void)]
+      [`(false)
+        (void)]
+      [`(not ,p)
+        (void)]
+      [`(begin ,es ... ,p)
+        (void)]
+      [`(if ,p1 ,p2 ,p3)
+        (void)]
+      [`(,r ,l ,o)
+        (void)]))
+
+  (define (asm-pred-lang-v6-template-tail t)
+    (match t
+      [`(jump ,trg ,locs ...)
+        (void)]
+      [`(begin ,es ... ,t)
+        (void)]
+      [`(if ,p ,t1 ,t2)
+        (void)]))
+
+  (define (asm-pred-lang-v6-template-effect e)
+    (match e
+      [`(set! ,loc (,binop ,loc ,opand))
+        (void)]
+      [`(set! ,loc ,triv)
+        (void)]
+      [`(begin ,es ... ,e)
+        (void)]
+      [`(if ,p ,e1 ,e2)
+        (void)]
+      [`(return-point ,label ,tail)
+        (void)]))
+
+  (define (asm-pred-lang-v6-template-opand o)
+    (match o
+      [(? int64?)
+       (void)]
+      [loc (void)]))
+
+  (define (asm-pred-lang-v6-template-triv t)
+    (match t
+      [(? label?)
+       (void)]
+      [opand (void)]))
+
+  (define (asm-pred-lang-v6-template-loc l)
+    (match l
+      [(? aloc?)
+       (void)]
+      [rloc (void)]))
+
+  (define (asm-pred-lang-v6-template-trg t)
+    (match t
+      [(? label?)
+       (void)]
+      [loc (void)]))
+
+  (define (asm-pred-lang-v6-template-binop b)
+    (match b
+      ['* (void)]
+      ['+ (void)]
+      ['- (void)]))
+  
+  (define (asm-pred-lang-v6-template-relop r)
+    (match r
+      ['< (void)]
+      ['<= (void)]
+      ['= (void)]
+      ['>= (void)]
+      ['> (void)]
+      ['!= (void)]))
+
+  (void))
