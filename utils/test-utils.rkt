e798e0aa2ddd4c56dbaeb7812cade075dc0e1189
emts <emts@student.ubc.ca>
2023-03-19 12:24:50 -0700

    m6: undead-analysis

--- utils/test-utils.rkt
+++ utils/test-utils.rkt
@@ -1,7 +1,12 @@
 #lang racket
 
+(require
+  rackunit
+  "gen-utils.rkt")
+
 (provide
-  extract-asm-pred-lang)
+  extract-asm-pred-lang
+  check-list-equiv?)
 
 ;; p -> info tail (List-of label) (List-of info) (List-of tail)
 ;; Extract fields from a Asm-Pred-Lang program
@@ -11,8 +16,12 @@
     [`(module ,main-info (define ,proc-labels ,proc-infos ,proc-tails) ... ,main-tail)
       (values main-info main-tail proc-labels proc-infos proc-tails)]))
 
+;; list list -> void
+;; Check if list contains the same elements
+(define-binary-check (check-list-equiv? list1 list2)
+  (list-equiv? list1 list2))
+
 (module+ test
-  (require rackunit)
   (define-values (main-info main-tail proc-labels proc-infos proc-tails)
     (extract-asm-pred-lang
       '(module ((locals ()))
@@ -24,4 +33,6 @@
   (check-equal? proc-labels '(L.test.1 L.test.2))
   (check-equal? proc-infos '(((locals (x.1))) ((locals (x.2)))))
   (check-equal? proc-tails '((halt 1) (halt 2)))
+
+  (check-list-equiv? '(1 3 2) '(3 2 1))
   )
