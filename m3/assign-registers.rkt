68da8e5e99f81395dedf9a0d6eebf314ae5752b0
Oussama Saoudi <oussama.sa216@gmail.com>
2023-03-21 16:50:27 -0700

    Added tests for local and assignments against interrogator

--- m3/assign-registers.rkt
+++ m3/assign-registers.rkt
@@ -51,7 +51,10 @@
           [(? register?) (append reg-list (list c))]
           [(? fvar?) reg-list]
           [(? aloc?)
-           (define prev (first (dict-ref prev-assignment c)))
+           (define prev 
+            (if (dict-has-key? prev-assignment c)
+              (first (dict-ref prev-assignment c))
+              (void)))
            (if (register? prev)
                (append (list prev) reg-list)
                ;; we don't need to add fvars from prev-assignment
@@ -136,6 +139,73 @@
                (set! t.6 (* t.6 p.1))
                (set! z.5 (+ z.5 t.6))
                (jump r15))))
+
+  (check-match
+    (assign-registers t2)
+    `(module
+      ((locals ,locals)
+      (conflicts
+        ((x.3 (z.5 p.1 y.4 v.1 w.2))
+        (w.2 (z.5 p.1 y.4 v.1 x.3))
+        (v.1 (w.2 x.3))
+        (y.4 (t.6 z.5 p.1 w.2 x.3))
+        (p.1 (t.6 z.5 y.4 w.2 x.3))
+        (z.5 (t.6 p.1 y.4 w.2 x.3))
+        (t.6 (z.5 p.1 y.4))))
+      (assignment
+        ,assignments))
+      (begin
+        (set! v.1 1)
+        (set! w.2 46)
+        (set! x.3 v.1)
+        (set! p.1 7)
+        (set! x.3 (+ x.3 p.1))
+        (set! y.4 x.3)
+        (set! p.1 4)
+        (set! y.4 (+ y.4 p.1))
+        (set! z.5 x.3)
+        (set! z.5 (+ z.5 w.2))
+        (set! t.6 y.4)
+        (set! p.1 -1)
+        (set! t.6 (* t.6 p.1))
+        (set! z.5 (+ z.5 t.6))
+        (jump r15)))
+    (and
+      (equal? locals `())
+      (equal? (list->set (map first assignments)) (list->set (map first `((p.1 r15) (z.5 r14) (y.4 r13) (x.3 r9) (w.2 r8) (t.6 r9) (v.1 r15)))))))
+
+  (check-match
+    (parameterize ([current-assignable-registers '(r9 r10)]) (assign-registers t2))
+    `(module
+      ((locals ,locals)
+      (conflicts
+        ((x.3 (z.5 p.1 y.4 v.1 w.2))
+        (w.2 (z.5 p.1 y.4 v.1 x.3))
+        (v.1 (w.2 x.3))
+        (y.4 (t.6 z.5 p.1 w.2 x.3))
+        (p.1 (t.6 z.5 y.4 w.2 x.3))
+        (z.5 (t.6 p.1 y.4 w.2 x.3))
+        (t.6 (z.5 p.1 y.4))))
+      (assignment ,assignments))
+      (begin
+        (set! v.1 1)
+        (set! w.2 46)
+        (set! x.3 v.1)
+        (set! p.1 7)
+        (set! x.3 (+ x.3 p.1))
+        (set! y.4 x.3)
+        (set! p.1 4)
+        (set! y.4 (+ y.4 p.1))
+        (set! z.5 x.3)
+        (set! z.5 (+ z.5 w.2))
+        (set! t.6 y.4)
+        (set! p.1 -1)
+        (set! t.6 (* t.6 p.1))
+        (set! z.5 (+ z.5 t.6))
+        (jump r15)))
+      (and
+        (equal? (list->set (map first assignments)) (list->set (map first `((p.1 r10) (z.5 r9) (v.1 r10)))))
+        (equal? (list->set locals) (list->set `(t.6 w.2 x.3 y.4)))))
 
   (check-equal? (assign-registers '(module ((locals (x.1)) (conflicts ((x.1 ()))) (assignment ()))
                                            (begin
