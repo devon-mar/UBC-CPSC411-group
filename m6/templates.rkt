3287aba29a3a0cc69e6cfaf0018c1568a0cef660
Devon Mar <devonm@mdmm.ca>
2023-03-24 19:30:13 -0700

    Update templates

--- m6/templates.rkt
+++ m6/templates.rkt
@@ -266,3 +266,158 @@
       ['!= (void)]))
 
   (void))
+
+(define/contract (para-asm-lang-v6-template p)
+  (-> para-asm-lang-v6? any/c)
+
+  (define (para-asm-lang-v6-template-p p)
+    (match p
+      [`(begin ,s ...)
+        (void)]))
+
+  (define (para-asm-lang-v6-template-s s)
+    (match s
+      [`(set! ,loc (,b ,loc ,o))
+        (void)]
+      [`(set! ,loc ,triv)
+        (void)]
+      [`(jump ,trg)
+        (void)]
+      [`(with-label ,l ,s)
+        (void)]
+      [`(compare ,l ,o)
+        (void)]
+      [`(jump-if ,r ,t)
+        (void)]))
+
+  (define (para-asm-lang-v6-template-triv t)
+    (match t
+      [(? label?)
+       (void)]
+      [opand
+        (void)]))
+
+  (define (para-asm-lang-v6-template-opand o)
+    (match o
+      [(? int64?)
+       (void)]
+      [loc
+        (void)]))
+
+  (define (para-asm-lang-v6-template-trg t)
+    (match t
+      [(? label?)
+       (void)]
+      [loc
+       (void)]))
+
+  (define (para-asm-lang-v6-template-loc l)
+    (match l
+      [(? register?)
+       (void)]
+      [addr (void)]))
+
+  (define (para-asm-lang-v6-template-binop b)
+    (match b
+      ['* (void)]
+      ['+ (void)]
+      ['- (void)]))
+
+  (define (para-asm-lang-v6-template-relop r)
+    (match r
+      ['< (void)]
+      ['<= (void)]
+      ['= (void)]
+      ['>= (void)]
+      ['> (void)]
+      ['!= (void)]))
+
+  (void))
+
+(define/contract (paren-x64-v6-template p)
+  (-> paren-x64-v6? any/c)
+
+  (define/contract (trg? t)
+    (-> any/c boolean?)
+    (or (register? t) (label? t)))
+
+  (define/contract (triv? t)
+    (-> any/c boolean?)
+    (or (trg? t) (int64? t)))
+
+  (define (paren-x64-v6-template-p p)
+    (match p
+      [`(begin ,s ...)
+        (void)]))
+
+  (define (paren-x64-v6-template-s s)
+    (match s
+      [`(set! ,reg (,b ,reg ,int32))
+        #:when (int32? int32)
+        (void)]
+      [`(set! ,reg (,b ,reg ,loc))
+        (void)]
+      [`(set! ,reg ,triv)
+        #:when (and (register? reg) (triv? triv))
+        (void)]
+      [`(set! ,reg ,loc)
+        #:when (register? reg)
+        (void)]
+      [`(set! ,addr ,int32)
+        #:when (int32? int32)
+        (void)]
+      [`(set! ,addr ,trg)
+        (void)]
+      [`(with-label ,label ,s)
+        (void)]
+      [`(jump ,trg)
+        (void)]
+      [`(compare ,reg ,opand)
+        (void)]
+      [`(jump-if ,relop ,label)
+        (void)]))
+
+  (define (paren-x64-v6-template-trg t)
+    (match t
+      [(? register?)
+        (void)]
+      [(? label?)
+       (void)]))
+
+  (define (paren-x64-v6-template-triv t)
+    (match t
+      [(? int64?)
+       (void)]
+      [trg 
+        (void)]))
+
+  (define (paren-x64-v6-template-opand o)
+    (match o
+      [(? int64?)
+       (void)]
+      [(? register?)
+       (void)]))
+
+  (define (paren-x64-v6-template-loc l)
+    (match l
+      [(? register?)
+       (void)]
+      [addr
+        (void)]))
+
+  (define (paren-x64-v6-template-binop b)
+    (match b
+      ['* (void)]
+      ['+ (void)]
+      ['- (void)]))
+
+  (define (paren-x64-v6-template-relop r)
+    (match r
+      ['< (void)]
+      ['<= (void)]
+      ['= (void)]
+      ['>= (void)]
+      ['> (void)]
+      ['!= (void)]))
+
+  (void))
