9abb86ba47968252ef9b819246be59ae3fe9bca7
emts <emts@student.ubc.ca>
2023-03-26 12:21:09 -0700

    m6: flatten-program, resolve-predicates

--- m4/flatten-program.rkt
+++ m4/flatten-program.rkt
@@ -1,26 +1,26 @@
 #lang racket
 
-(require cpsc411/langs/v4)
+(require cpsc411/langs/v6)
 
 (provide flatten-program)
 
 ;; Milestone 4 Exercise 6
-;; Compile Block-asm-lang v4 to Para-asm-lang v4
+;;
+;; Compile Block-asm-lang to Para-asm-lang
 ;; by flattening basic blocks into labeled instructions.
 (define/contract (flatten-program p)
-  (-> block-asm-lang-v4? para-asm-lang-v4?)
+  (-> block-asm-lang-v6? para-asm-lang-v6?)
 
   ;; Unused
-  #; 
+  #;
   (define (flatten-effect e)
     (match e
       [`(set! ,_ ,_) e]
       [`(set! ,_ (,_ ,_ ,_)) e]))
 
-  ;; (Block-Asm-Lang-v4 tail) -> (List-of (Para-Asm-Lang-v4 s))
+  ;; (Block-Asm-Lang-v6 tail) -> (List-of (Para-Asm-Lang-v6 s))
   (define (flatten-tail t)
     (match t
-      [`(halt ,_) (list t)]
       [`(jump ,_) (list t)]
       [`(begin ,effect ... ,tail)
        (append effect (flatten-tail tail))]
@@ -29,7 +29,7 @@
          (jump-if ,relop ,trg1)
          (jump ,trg2))]))
 
-  ;; (Block-Asm-Lang-v4 b) -> (List-of (Para-Asm-Lang-v4 s))
+  ;; (Block-Asm-Lang-v6 b) -> (List-of (Para-Asm-Lang-v6 s))
   (define (flatten-b b)
     (match b
       [`(define ,label ,tail)
@@ -47,9 +47,9 @@
   ;; base cases
   (check-equal?
     (flatten-program
-      '(module (define L.test.1 (halt 0))))
-    '(begin (with-label L.test.1 (halt 0))))
-  
+      '(module (define L.test.1 (jump done))))
+    '(begin (with-label L.test.1 (jump done))))
+
   ;; nested begins
   (check-equal?
     (flatten-program
@@ -60,17 +60,19 @@
             (set! rcx 9)
             (begin
               (set! rdx (* rdx 3))
-              (set! fv0 rcx)
-              (set! fv0 (+ fv0 rdx))
-              (halt fv0))))))
+              (set! (rbp - 0) rcx)
+              (set! (rbp - 0) (+ (rbp - 0) rdx))
+              (set! rax (rbp - 0))
+              (jump done))))))
     '(begin
       (with-label L.test.1 (set! rdx 14))
       (set! rcx 9)
       (set! rdx (* rdx 3))
-      (set! fv0 rcx)
-      (set! fv0 (+ fv0 rdx))
-      (halt fv0)))
-  
+      (set! (rbp - 0) rcx)
+      (set! (rbp - 0) (+ (rbp - 0) rdx))
+      (set! rax (rbp - 0))
+      (jump done)))
+
   ;; basic if
   (check-equal?
     (flatten-program
@@ -98,9 +100,9 @@
           (begin
             (set! rdx 144)
             (set! r9 10)
-            (set! fv0 L.test.2)
-            (if (< r9 4) (jump fv0) (jump L.test.5))))
-        (define L.test.4 (halt 5))
+            (set! (rbp - 0) L.test.2)
+            (if (< r9 4) (jump (rbp - 0)) (jump L.test.5))))
+        (define L.test.4 (jump done))
         (define L.test.5 (if (= rdx r9) (jump L.test.4) (jump L.test.2)))))
     '(begin
       (with-label L.test.1 (jump L.test.3))
@@ -108,11 +110,11 @@
       (jump rdx)
       (with-label L.test.3 (set! rdx 144))
       (set! r9 10)
-      (set! fv0 L.test.2)
+      (set! (rbp - 0) L.test.2)
       (compare r9 4)
-      (jump-if < fv0)
+      (jump-if < (rbp - 0))
       (jump L.test.5)
-      (with-label L.test.4 (halt 5))
+      (with-label L.test.4 (jump done))
       (with-label L.test.5 (compare rdx r9))
       (jump-if = L.test.4)
       (jump L.test.2)))
--- m4/resolve-predicates.rkt
+++ m4/resolve-predicates.rkt
@@ -1,28 +1,29 @@
 #lang racket
 
-(require cpsc411/compiler-lib)
+(require
+  cpsc411/compiler-lib
+  cpsc411/langs/v6)
 
 (provide resolve-predicates)
 
-;; Exercise 9
+;; Milestone 4 Exercise 9
+;;
 ;; Compiles preds in if statements to equivalent if statements or into
 ;; equivalent instructions
-;; block-pred-lang-v4? -> block-asm-lang-v4?
-(define (resolve-predicates p)
+(define/contract (resolve-predicates p)
+  (-> block-pred-lang-v6? block-asm-lang-v6?)
+
   ;; effect -> effect
   (define (convert-effect e)
     (match e
-      [`(set! ,loc ,triv) `(set! ,loc ,triv)]
-      [`(set! ,loc_1 (,binop ,loc_1 ,opand)) `(set! ,loc_1 (,binop ,loc_1 ,opand))]))
+      [`(set! ,loc ,triv) e]
+      [`(set! ,loc_1 (,binop ,loc_1 ,opand)) e]))
 
   ;; tail -> tail
   (define (convert-tail t)
     (match t
-      [`(halt ,opand) `(halt ,opand)]
       [`(jump ,trg) `(jump ,trg)]
-      [`(begin
-          ,effects ...
-          ,tail)
+      [`(begin ,effects ... ,tail)
        `(begin
           ,@(map convert-effect effects)
           ,(convert-tail tail))]
@@ -31,10 +32,10 @@
   ;; (if pred (jump trg) (jump trg)) -> (jump trg) | (if pred (jump trg) (jump trg))
   (define (convert-if pred t1 t2)
     (match pred
-      [`(,relop ,loc ,opand) `(if (,relop ,loc ,opand) ,t1 ,t2)]
       [`(true) t1]
       ['(false) t2]
-      [`(not ,nested-pred) (convert-if nested-pred t2 t1)]))
+      [`(not ,nested-pred) (convert-if nested-pred t2 t1)]
+      [`(,relop ,loc ,opand) `(if (,relop ,loc ,opand) ,t1 ,t2)]))
 
   ;; b -> b
   (define (convert-b b)
@@ -49,76 +50,103 @@
   (require rackunit)
   ;; No preds is identity function
   (define in1
-    '(module (define start
-               (begin
-                 (set! rsp 4)
-                 (halt rsp)))))
+    '(module
+      (define L.start.1
+        (begin
+          (set! rax 4)
+          (jump done)))))
   (define out1 in1)
 
   ;; Check that if true, replace predicate with the first
   ;; statement
   (define in2
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (true) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (true) (jump L.A.1) (jump L.B.1)))))
   (define out2
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (jump A))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (jump L.A.1))))
 
   ;; Check if false, replace predicate with second statement
   ;; in turnary
   (define in3
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (false) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (false) (jump L.A.1) (jump L.B.1)))))
   (define out3
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (jump B))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (jump L.B.1))))
 
   ;; Check handling not correctly
   (define in4
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (not (< 1 2)) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (not (< rax 2)) (jump L.A.1) (jump L.B.1)))))
   (define out4
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (< 1 2) (jump B) (jump A)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (< rax 2) (jump L.B.1) (jump L.A.1)))))
 
   ;; Check handling not not correctly
   (define in5
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (not (not (< 1 2))) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (not (not (< rax 2))) (jump L.A.1) (jump L.B.1)))))
   (define out5
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (< 1 2) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (< rax 2) (jump L.A.1) (jump L.B.1)))))
 
   ;; Identity function when pred must be evaluated
   (define in6
-    '(module (define A (halt 1)) (define B (halt 2))
-       (define start (if (< 1 2) (jump A) (jump B)))))
+    '(module
+      (define L.A.1 (jump done))
+      (define L.B.1 (jump done))
+      (define L.start.1 (if (< rax 2) (jump L.A.1) (jump L.B.1)))))
   (define out6 in6)
 
-  ;; Identity function on effect `(set! loc triv) 
+  ;; Identity function on effect `(set! loc triv)
   (define in7
-    '(module (define A (begin (set! fv0 2) (halt fv0)))))
+    '(module
+      (define L.A.1 (begin (set! (rbp - 0) 2) (jump done)))))
   (define out7 in7)
 
   ;; Identity function on effect `(set! loc_1 (+ loc_1 opand))
   (define in8
-    '(module (define A (begin (set! fv0 2) (set! fv0 (+ fv0 2)) (halt fv0)))))
+    '(module
+      (define L.A.1
+        (begin
+          (set! (rbp - 0) 2)
+          (set! (rbp - 0) (+ (rbp - 0) 2))
+          (jump done)))))
   (define out8 in8)
 
   ;; Identity function on effect `(set! loc_1 (+ loc_1 opand))
   (define in9
-    '(module (define A (begin (set! fv0 2) (set! fv0 (* fv0 2)) (halt fv0)))))
+    '(module
+      (define L.A.1
+        (begin
+          (set! (rbp - 0) 2)
+          (set! (rbp - 0) (* (rbp - 0) 2))
+          (jump done)))))
   (define out9 in9)
 
-  ;; Identity function on tail (halt opand) 
+  ;; Identity function on tail (jump opand)
   (define in10
-    '(module (define A (halt 3))))
+    '(module
+      (define L.A.1 (begin (set! (rbp - 8) done) (jump (rbp - 8))))
+      (define L.B.1 (jump L.A.1))))
   (define out10 in10)
-
-  ;; Identity function on tail (jump opand) 
-  (define in11
-    '(module (define A (begin (set! fv0 1) (halt fv0))) (define B (jump A))))
-  (define out11 in11)
 
   (check-equal? (resolve-predicates in1) out1)
   (check-equal? (resolve-predicates in2) out2)
@@ -129,6 +157,5 @@
   (check-equal? (resolve-predicates in7) out7)
   (check-equal? (resolve-predicates in8) out8)
   (check-equal? (resolve-predicates in9) out9)
-  (check-equal? (resolve-predicates in10) out10)
-  (check-equal? (resolve-predicates in11) out11))
+  (check-equal? (resolve-predicates in10) out10))
 
