9d0a65b5b9ed98a91c0111c79e1af6849acb7c4e
emts <emts@student.ubc.ca>
2023-04-01 12:30:34 -0700

    m7: uniquify

--- m2/uniquify.rkt
+++ m2/uniquify.rkt
@@ -18,8 +18,8 @@
 (define/contract (uniquify p)
   (-> exprs-lang-v7? exprs-unique-lang-v7?)
 
-  ;; value values-lang-v6-value
-  ;; -> values-unique-lang-v6-value
+  ;; value exprs-lang-v7-value
+  ;; -> exprs-unique-lang-v7-value
   (define/contract (uniquify-define dict name params value)
     (-> dict? name? (listof name?) any/c any/c)
     (define new-dict (foldl (lambda (x acc) (dict-set acc x (fresh x))) dict params))
@@ -39,8 +39,8 @@
       ,(map (lambda (x v) `[,(dict-ref new-dict x) ,(uniquify-value dict v)]) xs vs)
       ,(f new-dict body)))
 
-  ;; dict(name?, aloc?|label?) values-lang-v6-p -> values-unique-lang-v6-p
-  (define (uniquify-p dict p)
+  ;; dict(name?, aloc?|label?) exprs-lang-v7-p -> exprs-unique-lang-v7-p
+  (define/contract (uniquify-p dict p)
     (-> dict? any/c any/c)
     (match p
       [`(module (define ,x1s (lambda (,x2s ...) ,vs)) ... ,vt)
@@ -49,72 +49,62 @@
          ,@(map (lambda (name x2s value) (uniquify-define new-dict name x2s value)) x1s x2s vs)
          ,(uniquify-value new-dict vt))]))
 
-  ;; dict(name?, aloc?|label?) values-lang-v6-pred -> values-unique-lang-v6-pred
-  (define/contract (uniquify-pred dict p)
-    (-> dict? any/c any/c)
-    (match p
-      [`(true)
-       p]
-      [`(false)
-       p]
-      [`(not ,pred)
-       `(not ,(uniquify-pred dict pred))]
-      [`(let ([,xs ,vs] ...) ,pred)
-       (uniquify-let dict xs vs uniquify-pred pred)]
-      [`(if ,p1 ,p2 ,p3)
-       `(if
-         ,(uniquify-pred dict p1)
-         ,(uniquify-pred dict p2)
-         ,(uniquify-pred dict p3))]
-      [`(,relop ,t1 ,t2)
-       `(,relop
-         ,(uniquify-triv dict t1)
-         ,(uniquify-triv dict t2))]))
-
-  ;; dict(name?, aloc?|label?) values-lang-v6-value -> values-unique-lang-v6-value
+  ;; dict(name?, aloc?|label?) exprs-lang-v7-value -> exprs-unique-lang-v7-value
   (define/contract (uniquify-value dict v)
     (-> dict? any/c any/c)
     (match v
-      [`(if ,p ,v1 ,v2)
+      [`(if ,vp ,v1 ,v2)
        `(if
-         ,(uniquify-pred dict p)
+         ,(uniquify-value dict vp)
          ,(uniquify-value dict v1)
          ,(uniquify-value dict v2))]
       [`(let ([,xs ,vs] ...) ,v)
        (uniquify-let dict xs vs uniquify-value v)]
-      [`(call ,x ,vs ...)
+      [`(call ,vc ,vs ...)
        `(call
-         ,(dict-ref dict x)
+         ,(uniquify-value dict vc)
          ,@(map (lambda (v) (uniquify-value dict v)) vs))]
-      [`(,binop ,v1 ,v2)
-       `(,binop
-         ,(uniquify-value dict v1)
-         ,(uniquify-value dict v2))]
       [_ (uniquify-triv dict v)]))
 
-  ;; dict(name?, aloc?|label?) values-lang-v6-triv -> values-unique-lang-v6-triv
+  ;; dict(name?, aloc?|label?) exprs-lang-v7-triv -> exprs-unique-lang-v7-triv
   (define/contract (uniquify-triv dict t)
     (-> dict? any/c any/c)
+    ;; Merged template with x
     (match t
-      [(? int64?) t]
-      [(? name?) (dict-ref dict t)]))
+      ;; if prim-f|empty (or any symbol) gets shadowed by user-defined function
+      ;; it should be replaced with a label|aloc
+      [(? name?) (dict-ref dict t t)]
+      [(? int61?) t]
+      [#t t]
+      [#f t]
+      [`empty t]
+      [`(void) t]
+      [`(error ,_) t]
+      [(? ascii-char-literal?) t]
+      [prim-f prim-f]))
 
   #;
   (define (uniquify-binop b)
     (match b
       ['* (void)]
       ['+ (void)]
-      ['- (void)]))
-
-  #;
-  (define (uniquify-relop r)
-    (match r
+      ['- (void)]
+      ['eq? (void)]
       ['< (void)]
       ['<= (void)]
-      ['= (void)]
       ['>= (void)]
-      ['> (void)]
-      ['!= (void)]))
+      ['> (void)]))
+
+  #;
+  (define (uniquify-unop u)
+    (match u
+      ['fixnum? (void)]
+      ['boolean? (void)]
+      ['empty? (void)]
+      ['void? (void)]
+      ['ascii-char? (void)]
+      ['error? (void)]
+      ['not (void)]))
 
   (uniquify-p '() p))
 
@@ -122,6 +112,7 @@
   (require
    rackunit)
 
+  ;; Check that compiled program interprets to 42
   (define-check (check-42 p)
     (check-equal?
       (interp-exprs-unique-lang-v7 (uniquify p))
@@ -135,7 +126,7 @@
   ;; proc with args
   (check-42
     '(module
-       (define add (lambda (x y) (+ x y)))
+       (define add (lambda (x y) (call + x y)))
        (call add 40 2)))
 
   ;; proc calling another proc
@@ -144,19 +135,19 @@
        (define identity (lambda (x) x))
        (define foo
          (lambda (x y z)
-           (let ([xy (+ x y)])
+           (let ([xy (call + x y)])
              ;; call "down"
              (call bar xy z))))
        (define bar
          (lambda (x y)
-           (let ([xy (* x y)])
+           (let ([xy (call * x y)])
              ;; call "up'
              (call identity xy))))
        (call foo 20 1 2)))
 
   ;; M2 tests
   ; simple
-  (check-42 '(module (+ 40 2)))
+  (check-42 '(module (call + 40 2)))
 
   ; one var
   (check-42 '(module (let ([x 42]) x)))
@@ -167,7 +158,7 @@
     '(module
        (let ([x 40])
          (let ([y 2])
-           (+ x y)))))
+           (call + x y)))))
 
   ; shadow decl
   ; let in tail of let
@@ -175,49 +166,47 @@
       '(module
         (let ([x 20])
           (let ([x 21])
-            (+ x x)))))
+            (call + x x)))))
 
   ; let in value of let decl
   (check-42
     '(module
        (let ([x 2]
-             [y (let ([x 38]) (+ 2 x))])
-         (+ x y))))
+             [y (let ([x 38]) (call + 2 x))])
+         (call + x y))))
 
 
   ; nesting and shadowing
   (check-42
     '(module
        (let ([foo 20])
-         (let ([foo (+ 1 foo)]
-               [bar (+ 1 foo)])
-           (+ foo bar)))))
+         (let ([foo (call + 1 foo)]
+               [bar (call + 1 foo)])
+           (call + foo bar)))))
 
 
   ;; M4 tests
 
   (check-42
     '(module
-       ;; tail/(let ([x value] ...) tail)
+       ;; value/(let ([x value] ...) value)
        (let ([x 10]
-             ;; pred/(not pred)
-             ;; pred/(false)
-             [y (if (not (false)) 20 10)]
-             ;; values/(if pred value value)
-             ;; pred/(true)
-             [a (if (true) 42 0)]
+             ;; value/(if (call not value) ...)
+             [y (if (call not #f) 20 10)]
+             ;; value/(if value value value)
+             [a (if #t 42 0)]
              [b 1])
-         ;; pred/(relop triv triv)
-         (if (= x y)
+         ;; (if (call relop triv triv) ...)
+         (if (call eq? x y)
            b
            a))))
 
   (check-42
     '(module
-       ;; pred/(let ([x value] ...) pred)
+       ;; (if (let ([x value] ...) ...)
        (if (let ([x 2]
                  [y 4])
-             (< x y))
+             (call < x y))
          42
          0)))
 
@@ -227,31 +216,110 @@
 
   (check-42
     '(module
-       ;; values/(let ([x value] ...) value)
-       (let ([x (let ([y 40]) (+ y 1))]
+       ;; value/(let ([x value] ...) value)
+       (let ([x (let ([y 40]) (call + y 1))]
              [y 1])
-         ;; values/(binop triv triv)
-         (+ x y))))
+         ;; value/(call binop triv triv)
+         (call + x y))))
 
   (check-42
     '(module
-       (if (if (true) (false) (true))
+       (if (if #t #f #t)
          20
          42)))
 
   ;; M6 tests
   ;; minus
-  (check-42 '(module (- -20 -62)))
+  (check-42 '(module (call - -20 -62)))
 
   ;; call as value
   (check-42
     '(module
       (define fact
         (lambda (n acc)
-          (if (<= n 1)
+          (if (call <= n 1)
               acc
-              (let ([n (- n 1)]
-                    [acc (* n acc)])
+              (let ([n (call - n 1)]
+                    [acc (call * n acc)])
                 (let ([r (call fact n acc)]) r)))))
-      (let ([x (call fact 4 1)]) (+ x 18))))
+      (let ([x (call fact 4 1)]) (call + x 18))))
+
+  ;; M7 tests
+  ;; value in if
+  (check-42
+    '(module (if (error 1) (if empty 42 -2) -1)))
+
+  ;; value in call
+  (check-42
+    '(module
+      (call + (call * (call * (let ([x 2]) x) 4) (call (if #t - eq?) -17 5)) 218)))
+
+  ;; binops
+  (check-42
+    '(module
+      (let ([x (call + 4 2)]      ; +   =>    6
+            [y (call * 5 10)]     ; *   =>   50
+            [z (call - 9 2)])     ; -   =>  -18
+        (let ([a (call eq? x 6)]  ; eq? => true
+              [b (call < 0 z)]    ; <   => true
+              [c (call <= y 50)]  ; <=  => true
+              [d (call > -18 z)]  ; >   => false
+              [e (call >= x 10)]) ; >=  => false
+          (if a
+              (if b
+                  (if c
+                      (if d
+                          -4
+                          (if e
+                              -5
+                              (call - y (call + 2 x))))
+                      -3)
+                  -2)
+              -1)))))
+
+  ;; unops + data types
+  (check-42
+    '(module
+      (define all-true
+        (lambda (a b c d e f g)
+          (if a (if b (if c (if d (if e (if f (if g #t #f) #f) #f) #f) #f) #f) #f)))
+      (define all-false
+        (lambda (a b c d e f g)
+          (if a #f (if b #f (if c #f (if d #f (if e #f (if f #f (if g #f #t)))))))))
+      (let ([x 17]
+            [y 2]
+            [z 8])
+        (let ([ft (call fixnum? x)]
+              [ff (call fixnum? #t)]
+              [bt (call boolean? #f)]
+              [bf (call boolean? empty)]
+              [emt (call empty? empty)]
+              [emf (call empty? 0)]
+              [vt (call void? (void))]
+              [vf (call void? empty)]
+              [at (call ascii-char? #\D)]
+              [af (call ascii-char? x)]
+              [ert (call error? (error 2))]
+              [erf (call error? (void))]
+              [nt (call not #f)]
+              [nf (call not #t)])
+          (let ([tr (if (call all-true ft bt emt vt at ert nt)
+                        (call * x y)
+                        -1)]
+                [fr (if (call all-false ff bf emf vf af erf nf)
+                        (call - 0 z)
+                        -2)])
+            (call - tr fr))))))
+
+  ;; shadowing
+  (check-42
+    '(module
+      (define +
+        (lambda (eq? >=)
+          (let ([not *])
+            (call not eq? >=))))
+      (let ([- +]
+            [fixnum? (call - 24 3)]
+            [empty 2])
+        (call - fixnum? empty))))
   )
