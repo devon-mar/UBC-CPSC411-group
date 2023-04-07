4ceeb9b53387f94023b712c1ccda8b8f3fcf4b2a
Devon Mar <devonm@mdmm.ca>
2023-04-06 22:26:44 -0700

    Optimize `not`

--- m7/specify-representation.rkt
+++ m7/specify-representation.rkt
@@ -9,7 +9,7 @@
 (provide specify-representation)
 
 ;; Milestone 7 Exercise 5
-;; 
+;;
 ;; Compiles immediate data and primitive operations into their implementations
 ;; as ptrs and primitive bitwise operations on ptrs.
 (define/contract (specify-representation p)
@@ -22,29 +22,37 @@
   (define/contract (binop? b)
     (-> any/c boolean?)
     (and (memq
-           b
-           '(unsafe-fx*
-             unsafe-fx+
-             unsafe-fx-
-             eq?
-             unsafe-fx<
-             unsafe-fx<=
-             unsafe-fx>
-             unsafe-fx>=))
+          b
+          '(unsafe-fx*
+            unsafe-fx+
+            unsafe-fx-
+            eq?
+            unsafe-fx<
+            unsafe-fx<=
+            unsafe-fx>
+            unsafe-fx>=))
          #t))
 
   (define/contract (unop? u)
     (-> any/c boolean?)
     (and (memq
-           u
-           '(fixnum?
-             boolean?
-             empty?
-             void?
-             ascii-char?
-             error?
-             not))
+          u
+          '(fixnum?
+            boolean?
+            empty?
+            void?
+            ascii-char?
+            error?
+            not))
          #t))
+
+  ;; Takes a pred and returns a value that is equal to the true/false ptr.
+  ;;
+  ;; exprs-bits-lang-v7-pred -> exprs-bits-lang-v7-value
+  (define (pred->value p)
+    `(if ,p
+         ,(current-true-ptr)
+         ,(current-false-ptr)))
 
   ;; Generates (tag-type v) and (untag-type v) functions for the given type
   ;;
@@ -81,11 +89,7 @@
                      [tagp (format-id #'type "current-~a-tag" #'type)]
                      [maskp (format-id #'type "current-~a-mask" #'type)])
          ;; generates a exprs-bits-lang-v7-pred to check if v is of type name
-         #'(define (name v)
-             (-> any/c any/c)
-             `(if (= (bitwise-and ,v ,(maskp)) ,(tagp))
-                ,(current-true-ptr)
-                ,(current-false-ptr))))]))
+         #'(define (name v) `(= (bitwise-and ,v ,(maskp)) ,(tagp))))]))
   (make-unop->pred fixnum)
   (make-unop->pred boolean)
   (make-unop->pred empty)
@@ -100,37 +104,37 @@
     `(define
        ,label
        (lambda ,params
-         ,(specify-representation-value v))))
+         ,(specify-representation-value/value v))))
 
   ;; exprs-unsafe-data-lang-v7-p exprs-bits-lang-v7-p
   (define (specify-representation-p p)
     (match p
       [`(module (define ,labels (lambda (,alocs ...) ,values)) ... ,value)
-        `(module
-           ,@(map specify-representation-proc labels alocs values)
-           ,(specify-representation-value value))]))
+       `(module
+            ,@(map specify-representation-proc labels alocs values)
+          ,(specify-representation-value/value value))]))
 
-  ;; exprs-unsafe-data-lang-v7-p exprs-bits-lang-v7-p
-  (define (specify-representation-value v)
+  ;; exprs-unsafe-data-lang-v7-p exprs-bits-lang-v7-value
+  (define (specify-representation-value/value v)
     (match v
       ;; modified template - removed tail v
       [`(call ,vs ...)
-        `(call ,@(map specify-representation-value vs))]
+       `(call ,@(map specify-representation-value/value vs))]
       [`(let ([,as ,vs] ...) ,v)
-        `(let
-           ,(map (lambda (a v) `[,a ,(specify-representation-value v)]) as vs)
-           ,(specify-representation-value v))]
+       `(let
+            ,(map (lambda (a v) `[,a ,(specify-representation-value/value v)]) as vs)
+          ,(specify-representation-value/value v))]
       [`(if ,v1 ,v2 ,v3)
-        `(if (!= ,(specify-representation-value v1) ,(current-false-ptr))
-           ,(specify-representation-value v2)
-           ,(specify-representation-value v3))]
+       `(if ,(specify-representation-value/pred v1)
+            ,(specify-representation-value/value v2)
+            ,(specify-representation-value/value v3))]
       [`(,primop ,vs ...)
-        #:when (primop? primop)
-        (specify-representation-primop primop (map specify-representation-value vs))]
-      [_ (specify-representation-triv v)]))
+       #:when (primop? primop)
+       (specify-representation-primop/value primop vs)]
+      [_ (specify-representation-triv/value v)]))
 
   ;; exprs-unsafe-data-lang-v7-triv exprs-bits-lang-v7-triv
-  (define (specify-representation-triv t)
+  (define (specify-representation-triv/value t)
     (match t
       [(? label?)
        t]
@@ -147,72 +151,123 @@
       ['(void)
        (current-void-ptr)]
       [`(error ,uint8)
-        (tag-error uint8)]
+       (tag-error uint8)]
       [(? ascii-char-literal?)
        (tag-ascii-char t)]))
 
-  ;; exprs-unsafe-data-lang-v7-primop (listof exprs-bits-lang-v7-value) -> exprs-bits-lang-v7-pred
-  (define (specify-representation-primop p vs)
+  ;; exprs-unsafe-data-lang-v7-primop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-value
+  (define (specify-representation-primop/value p vs)
     (match p
       [(? binop?)
-       (specify-representation-binop p vs)]
+       (specify-representation-binop/value p vs)]
       [(? unop?)
-       (specify-representation-unop p vs)]))
+       (specify-representation-unop/value p vs)]))
 
-  ;; exprs-unsafe-data-lang-v7-binop (listof exprs-bits-lang-v7-value) -> exprs-bits-lang-v7-pred
-  (define/contract (specify-representation-binop b vs)
+  ;; exprs-unsafe-data-lang-v7-unop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-value
+  (define (specify-representation-unop/value u vs)
+    (match u
+      ['not
+       `(if ,(specify-representation-value/pred (car vs))
+            ,(current-false-ptr)
+            ,(current-true-ptr))]
+      [_ (pred->value (specify-representation-unop/pred u vs))]))
+
+  ;; exprs-unsafe-data-lang-v7-binop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-value
+  (define/contract (specify-representation-binop/value b vs)
     (-> symbol? (listof any/c) any/c)
+    ;; It is very important that this match doesn't contain cases
+    ;; that the /pred version matches!
     (match b
       ['unsafe-fx*
        (match vs
          [`(,i ,v)
-           #:when (int64? i)
-           `(* ,(untag-fixnum i) ,v)]
+          #:when (int64? i)
+          `(* ,i ,(specify-representation-value/value v))]
          [`(,v ,i)
-           #:when (int64? i)
-           `(* ,v ,(untag-fixnum i))]
+          #:when (int64? i)
+          `(* ,(specify-representation-value/value v) ,i)]
          [`(,v1 ,v2)
-           `(* ,v1 (arithmetic-shift-right ,v2 ,(current-fixnum-shift)))])]
+          `(* ,v1 (arithmetic-shift-right ,(specify-representation-value/value v2) ,(current-fixnum-shift)))])]
       ['unsafe-fx+
-       `(+ ,@vs)]
+       `(+ ,@(map specify-representation-value/value vs))]
       ['unsafe-fx-
-       `(- ,@vs)]
-      ;; modfied template - the rest all need to be converted to (if (relop ,vs) true false)
-      ;; so a nested match was added
-      [_
-        (define r
-          (match b
-            ['eq? '=]
-            ['unsafe-fx< '<]
-            ['unsafe-fx<= '<=]
-            ['unsafe-fx> '>]
-            ['unsafe-fx>= '>=]))
-        `(if (,r ,@vs)
-           ,(current-true-ptr)
-           ,(current-false-ptr))]))
+       `(- ,@(map specify-representation-value/value vs))]
+      ;; modfied template - squashed cases - the rest all need to be converted
+      ;; to (if (relop ,vs) true false)
+      [_ (pred->value (specify-representation-binop/pred b vs))]))
 
+  ;; The following functions' return value may only be used in pred position of
+  ;; the target language.
+  ;; exprs-unsafe-data-lang-v7-p exprs-bits-lang-v7-pred
+  (define (specify-representation-value/pred v)
+    (match v
+      [`(call ,vs ...)
+       `(!= (call ,@(map specify-representation-value/value vs)) ,(current-false-ptr))]
+      [`(let ([,as ,vs] ...) ,v)
+       `(let
+            ,(map (lambda (a v) `[,a ,(specify-representation-value/value v)]) as vs)
+          ,(specify-representation-value/pred v))]
+      [`(if ,v1 ,v2 ,v3)
+       `(if ,(specify-representation-value/pred v1)
+            ,(specify-representation-value/pred v2)
+            ,(specify-representation-value/pred v3))]
+      [`(,primop ,vs ...)
+       #:when (primop? primop)
+       (specify-representation-primop/pred primop vs)]
+      [_ (specify-representation-triv/pred v)]))
 
-  ;; exprs-unsafe-data-lang-v7-unop (listof exprs-bits-lang-v7-value) -> exprs-bits-lang-v7-pred
-  (define (specify-representation-unop u vs)
+  ;; exprs-unsafe-data-lang-v7-triv exprs-bits-lang-v7-pred
+  (define (specify-representation-triv/pred t)
+    ;; Modified template - we return #f is t==#f, otherwise #t
+    (match t
+      [#f `(false)]
+      [_ `(true)]))
+
+  ;; exprs-unsafe-data-lang-v7-primop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-pred
+  (define (specify-representation-primop/pred p vs)
+    (match p
+      [(? binop?)
+       (specify-representation-binop/pred p vs)]
+      [(? unop?)
+       (specify-representation-unop/pred p vs)]))
+
+  ;; exprs-unsafe-data-lang-v7-binop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-pred
+  (define/contract (specify-representation-binop/pred b vs)
+    (-> symbol? (listof any/c) any/c)
+    (match b
+      ;; It is very important that this match doesn't contain cases
+      ;; that the /value version matches!
+      ['eq? `(= ,@(map specify-representation-value/value vs))]
+      ['unsafe-fx< `(< ,@(map specify-representation-value/value vs))]
+      ['unsafe-fx<= `(<= ,@(map specify-representation-value/value vs))]
+      ['unsafe-fx> `(> ,@(map specify-representation-value/value vs))]
+      ['unsafe-fx>= `(>= ,@(map specify-representation-value/value vs))]
+      ;; Modified template - the rest all operate on and return fixnums (hopefully)
+      ;; TODO
+      ;; The pass before this pass is `implement-safe-primops` and should make sure
+      ;; that all arguments to unsafe-fx[+-*] so this should always be true ...
+      #;
+      [_ `(true)]
+      ;; but just in case ...
+      [_ `(!= ,(specify-representation-binop/value b vs), (current-false-ptr))]))
+
+  ;; exprs-unsafe-data-lang-v7-unop (listof exprs-unsafe-data-lang-v7-value) -> exprs-bits-lang-v7-pred
+  (define (specify-representation-unop/pred u vs)
     (define v (car vs))
     (match u
-      ['fixnum?
-       (fixnum?->pred v)]
-      ['boolean?
-       (boolean?->pred v)]
-      ['empty?
-       (empty?->pred v)]
-      ['void?
-       (void?->pred v)]
-      ['ascii-char?
-       (ascii-char?->pred v)]
-      ['error?
-       (error?->pred v)]
       ['not
-       ;; TODO we can optimise this by using not (see reference compiler)...
-       `(if (!= ,v ,(current-false-ptr))
-          ,(current-false-ptr)
-          ,(current-true-ptr))]))
+       `(not ,(specify-representation-value/pred v))]
+      ;; modified template - added nested match since the rest need
+      ;; (specify-representation-value/value v)
+      [_
+       ((match u
+          ['fixnum? fixnum?->pred]
+          ['boolean? boolean?->pred]
+          ['empty? empty?->pred]
+          ['void? void?->pred]
+          ['ascii-char? ascii-char?->pred]
+          ['error? error?->pred])
+        (specify-representation-value/value v))]))
 
   (specify-representation-p p))
 
@@ -226,22 +281,23 @@
 
   (define-check (check-eval-true p)
     (check-equal?
-      (interp-exprs-bits-lang-v7 (specify-representation p))
-      (current-true-ptr)
-      (format "expected true: ~a" p)))
+     (interp-exprs-bits-lang-v7 (specify-representation p))
+     (current-true-ptr)
+     (format "expected true: ~a" p)))
 
   (define-check (check-eval-false p)
     (check-equal?
-      (interp-exprs-bits-lang-v7 (specify-representation p))
-      (current-false-ptr)
-      (format "expected false ~a" p)))
+     (interp-exprs-bits-lang-v7 (specify-representation p))
+     (current-false-ptr)
+     (format "expected false ~a" p)))
 
   (define-syntax-rule (triv-test i o)
     (check-equal?
-      (specify-representation `(module ,i))
-      `(module ,o)))
+     (specify-representation `(module ,i))
+     `(module ,o)))
   ;; triv/label
   (triv-test 'L.a.1 'L.a.1)
+
   ;; triv/aloc
   (triv-test 'x.1 'x.1)
   ;; triv/fixnum
@@ -278,26 +334,26 @@
 
   ;; lhs is fixnum
   (check-42
-    '(module
-       (let ([x.1 21])
-         (unsafe-fx* 2 x.1))))
+   '(module
+      (let ([x.1 21])
+        (unsafe-fx* 2 x.1))))
   ;; rhs is fixnum
   (check-42
-    '(module
-       (let ([x.1 2])
-         (unsafe-fx* x.1 21))))
+   '(module
+      (let ([x.1 2])
+        (unsafe-fx* x.1 21))))
   ;; neither are fixnums
   (check-42
-    '(module
-       (let ([x.1 2]
-             [y.1 21])
-         (unsafe-fx* x.1 y.1))))
+   '(module
+      (let ([x.1 2]
+            [y.1 21])
+        (unsafe-fx* x.1 y.1))))
 
   (define-syntax-rule (relop-test b tx ty fx fy)
     (test-case
-      (format "relop ~a" 'b)
-      (check-eval-true '(module (b tx ty)))
-      (check-eval-false '(module (b fx fy)))))
+     (format "relop ~a" 'b)
+     (check-eval-true '(module (b tx ty)))
+     (check-eval-false '(module (b fx fy)))))
 
   ;; binop/eq?
   (relop-test eq? 42 42 1 2)
@@ -316,15 +372,15 @@
   (define-syntax-rule (unop-test unop t fs ...)
     (begin
       (check-equal?
-        (interp-exprs-bits-lang-v7
-          (specify-representation '(module (unop t))))
-        (current-true-ptr)
-        (format "expected true: (~a ~a)" 'unop 't))
+       (interp-exprs-bits-lang-v7
+        (specify-representation '(module (unop t))))
+       (current-true-ptr)
+       (format "expected true: (~a ~a)" 'unop 't))
       (check-equal?
-        (interp-exprs-bits-lang-v7
-          (specify-representation '(module (unop fs))))
-        (current-false-ptr)
-        (format "expected false: (~a ~a)" 'unop 'fs)) ...))
+       (interp-exprs-bits-lang-v7
+        (specify-representation '(module (unop fs))))
+       (current-false-ptr)
+       (format "expected false: (~a ~a)" 'unop 'fs)) ...))
 
   ;; unop/fixnum?
   (unop-test fixnum? 1 #t #f empty (void) #\a (error 1))
@@ -351,15 +407,9 @@
 
   ;; value/(if value value value)
   (check-42
-    '(module
-       (if 10
-         42
-         0)))
+    '(module (if 10 42 0)))
   (check-42
-    '(module
-       (if #f
-         0
-         42)))
+    '(module (if #f 0 42)))
   (check-42
     '(module
        (if (eq? 10 10)
@@ -387,11 +437,28 @@
 
   ;; value/(let ([aloc value] ...) value)
   (check-42
+   '(module
+        (let([x.1 8])
+          (let ([a.1 (unsafe-fx* x.1 3)]
+                [b.1 18])
+            (unsafe-fx+ a.1 b.1)))))
+
+  ;; nested ifs
+  (check-42
+    '(module (if (if (eq? 10 20) (eq? 10 11) (eq? 1 1)) 42 0)))
+
+  ;; let in pred position
+  (check-42
     '(module
-       (let([x.1 8])
-         (let ([a.1 (unsafe-fx* x.1 3)]
-               [b.1 18])
-           (unsafe-fx+ a.1 b.1)))))
+       (if (let ([x.1 10]) (eq? x.1 10))
+         42
+         0)))
+
+  ;; call in pred position
+  (check-42
+    '(module
+       (define L.myeq.1 (lambda (x.1 y.1) (eq? x.1 y.1)))
+       (if (call L.myeq.1 10 20)
+         0
+         42)))
   )
- 	 	 	 	 
-  
