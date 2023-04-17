dc803e911e795a2fbdde3ba8c80ab9c1e9579d70
emts <emts@student.ubc.ca>
2023-04-16 20:19:33 -0700

    E2E tests

--- e2e-test.rkt
+++ e2e-test.rkt
@@ -84,8 +84,11 @@
   ;; lambda
   (check-execute
     '(module
-      (define func1 (lambda (fn x y) (call (call fn x y) (call * x y))))
-      (let ([func2 (lambda (x y) (lambda (a) (call - a (call + x (call * y y)))))])
+      (define func1
+        (lambda (fn x y)
+          (call (call fn x y) (call * x y))))
+      (let ([func2 (lambda (x y)
+                     (lambda (a) (call - a (call + x (call * y y)))))])
         (call func1 func2 17 7)))
     53)
 
@@ -100,6 +103,7 @@
                     [acc (call * acc n)])
                 (call fact n acc)))))
       (let ([x (call fact 8 1)]
-            [y (call fact 5 1)]) (call + x y)))
+            [y (call fact 5 1)])
+        (call + x y)))
     40440)
   )
